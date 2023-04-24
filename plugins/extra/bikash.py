4f17010db772a4b41105e92e2ffadabe4d2463b6
Bikash Halder <115603304+BikashHalderNew@users.noreply.github.com>
2023-04-24 21:19:48 +0530

    Update bikash.py

--- plugins/extra/bikash.py
+++ plugins/extra/bikash.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 
 from Bikash import app
-from Bikash.utils
+from Bikash.utils.bgtmusic.bk import command
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 
