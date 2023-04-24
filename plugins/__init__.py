676f5c7fcc9ce1c1ecb20083e84d319828616af5
Bikash Halder <115603304+BikashHalderNew@users.noreply.github.com>
2023-04-24 19:57:30 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,14 +1,22 @@
+# Power By @BikashHalder & @AdityaHalder 
+# Join @BikashGadgetsTech For More Update
+# Join @AdityaCheats For Hack
+# Join Our Chats @Bgt_Chat & @Adityadiscus 
+
 import glob
-from os.path import basename, dirname, isfile
+from os.path import dirname, isfile
 
 
 def __list_all_modules():
-    mod_paths = glob.glob(dirname(__file__) + "/*/*.py")
+    work_dir = dirname(__file__)
+    mod_paths = glob.glob(work_dir + "/*/*.py")
 
     all_modules = [
-        basename(f)[:-3]
+        (((f.replace(work_dir, "")).replace("/", "."))[:-3])
         for f in mod_paths
-        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+        if isfile(f)
+        and f.endswith(".py")
+        and not f.endswith("__init__.py")
     ]
 
     return all_modules
@@ -16,3 +24,10 @@ def __list_all_modules():
 
 ALL_MODULES = sorted(__list_all_modules())
 __all__ = ALL_MODULES + ["ALL_MODULES"]
+
+
+
+# Power By @BikashHalder & @AdityaHalder 
+# Join @BikashGadgetsTech For More Update
+# Join @AdityaCheats For Hack
+# Join Our Chats @Bgt_Chat & @Adityadiscus 
