3f953df4efd0314ad2ccf260a128517325b3e329
Bikash Halder <115603304+BikashHalderNew@users.noreply.github.com>
2023-04-24 20:59:17 +0530

    Update start.py

--- plugins/bot/start.py
+++ plugins/bot/start.py
@@ -18,7 +18,7 @@ from Bikash.Bgt import get_command, get_string
 from Bikash import Telegram, YouTube, app
 from Bikash.misc import SUDOERS, _boot_
 from plugins.play.playlist import del_plist_msg
-from plugins.sudoers import sudoers_list
+from plugins.sudo.sudoers import sudoers_list
 from Bikash.utils.database import (add_served_chat,
                                        add_served_user,
                                        get_served_chats,
