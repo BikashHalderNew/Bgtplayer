19f0afba0f53b138f83ddeea8cd2456b3b36c710
Bikash Halder <115603304+BikashHalderNew@users.noreply.github.com>
2023-04-24 20:18:44 +0530

    Update mute.py

--- plugins/admins/mute.py
+++ plugins/admins/mute.py
@@ -9,7 +9,6 @@ from pyrogram.types import Message
 from Bikash.config import BANNED_USERS
 from Bikash import app
 from Bikash.core.call import Bikashh
-from Bikash.utils.bgtmusic.bk import command
 from Bikash.utils.database import is_muted, mute_on
 from Bikash.utils.decorators import AdminRightsCheck
 
