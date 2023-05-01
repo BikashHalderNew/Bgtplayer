82240e0d42a73082c6ae4c60592903b6b2ddbf52
ƛɗɩtyɑ Ɦɑɭɗɘr <131466403+AdityaHalderOS@users.noreply.github.com>
2023-05-01 14:01:36 +0530

    Update thumbnails.py

--- Bikash/utils/thumbnails.py
+++ Bikash/utils/thumbnails.py
@@ -33,9 +33,77 @@ def changeImageSize(maxWidth, maxHeight, image):
 
 
 async def gen_thumb(videoid, user_id):
-    if os.path.isfile(f"cache/{videoid}.png"):
-        return f"cache/{videoid}.png"
+    try:
+        os.remove(f"cache/thumb{videoid}.png")
+    except:
+        pass
+    url = f"https://www.youtube.com/watch?v={videoid}"
+    try:
+        results = VideosSearch(url, limit=1)
+        for result in (await results.next())["result"]:
+            try:
+                title = result["title"]
+                title = re.sub("\W+", " ", title)
+                title = title.title()
+            except:
+                title = "Unsupported Title"
+            try:
+                duration = result["duration"]
+            except:
+                duration = "Unknown Mins"
+            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+            try:
+                views = result["viewCount"]["short"]
+            except:
+                views = "Unknown Views"
+            try:
+                channel = result["channel"]["name"]
+            except:
+                channel = "Unknown Channel"
 
+        async with aiohttp.ClientSession() as session:
+            async with session.get(thumbnail) as resp:
+                if resp.status == 200:
+                    f = await aiofiles.open(
+                        f"cache/thumb{videoid}.png", mode="wb"
+                    )
+                    await f.write(await resp.read())
+                    await f.close()
+
+        images = random.choice(themes)
+        image1 = Image.open(f"cache/thumb{videoid}.png")
+        image2 = Image.open(f"resource/{images}.png")
+        image3 = changeImageSize(1280, 720, image1)
+        image4 = changeImageSize(1280, 720, image2)
+        image5 = image3.convert("RGBA")
+        image6 = image4.convert("RGBA")
+        Image.alpha_composite(image5, image6).save("cache/temp.png")
+        img = Image.open("cache/temp.png")
+        draw = ImageDraw.Draw(img)
+        font = ImageFont.truetype("resource/font.otf", 32)
+        draw.text((190, 550), f"Title: {title[:50]} ...", (255, 255, 255), font=font)
+        draw.text((190, 590), f"Duration: {duration}", (255, 255, 255), font=font)
+        draw.text((190, 630), f"Views: {views}", (255, 255, 255), font=font)
+        draw.text((190, 670), f"Powered By: Bikash & Aditya Halder (@BikashHalder @AdityaHalder)", (255, 255, 255), font=font)
+        try:
+            os.remove(f"cache/thumb{videoid}.png")
+            os.remove(f"cache/temp.png")
+        except:
+            pass
+        img.save(f"cache/{videoid}.png")
+        return f"cache/{videoid}.png"
+    except Exception as e:
+        print(e)
+        return YOUTUBE_IMG_URL
+        
+        
+        
+        
+async def gen_qthumb(videoid, user_id):
+    try:
+        os.remove(f"cache/thumb{videoid}.png")
+    except:
+        pass
     url = f"https://www.youtube.com/watch?v={videoid}"
     try:
         results = VideosSearch(url, limit=1)
