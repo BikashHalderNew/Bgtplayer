f10d3a88ceafc2e56756b276eba2087dbfd5988e
Bikash Halder <115603304+BikashHalderNew@users.noreply.github.com>
2023-08-04 22:46:47 +0530

    Update userinfo.py

--- plugins/extra/userinfo.py
+++ plugins/extra/userinfo.py
@@ -9,7 +9,7 @@ def ids(_, message):
     reply = message.reply_to_message
     if reply:
         message.reply_text(
-            f"🥀 𝐃𝐞𝐚𝐫 𝐔𝐬𝐞𝐫 𝐓𝐡𝐢𝐬 𝐈𝐬 𝐔𝐬𝐞𝐫 𝐈𝐧𝐟𝐨 𝐒𝐞𝐞 𝐁𝐞𝐥𝐨𝐰 𝐈𝐧𝐟𝐨 🥀 \n\n🌿 𝐔𝐬𝐞𝐫 𝐈𝐝 🌿: `{message.from_user.id}`\n🥀 𝐅𝐢𝐫𝐬𝐭 𝐍𝐚𝐦𝐞 🍁 : {reply.from_user.first_name}' \n🥀 𝐔𝐬𝐞𝐫𝐍𝐚𝐦𝐞 🍁 : `{reply.from_user.id}`\n🥀 𝐂𝐡𝐚𝐭 𝐈𝐝 🥀 : `{message.chat.id}` \n\n𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 [𝐁𝐢𝐤𝐚𝐬𝐡 𝐆𝐚𝐝𝐠𝐞𝐭𝐬 𝐓𝐞𝐜𝐡](https://t.me/BikashGadgetsTech)"
+            f"🥀 𝐃𝐞𝐚𝐫 𝐔𝐬𝐞𝐫 𝐓𝐡𝐢𝐬 𝐈𝐬 𝐔𝐬𝐞𝐫 𝐈𝐧𝐟𝐨 𝐒𝐞𝐞 𝐁𝐞𝐥𝐨𝐰 𝐈𝐧𝐟𝐨 🥀 \n\n🌿 𝐔𝐬𝐞𝐫 𝐈𝐝 🌿: `{message.from_user.id}`\n🥀 𝐅𝐢𝐫𝐬𝐭 𝐍𝐚𝐦𝐞 🍁 : {reply.from_user.first_name} \n\n🥀 𝐔𝐬𝐞𝐫𝐍𝐚𝐦𝐞 🍁 : `{reply.from_user.id}`\n🥀 𝐂𝐡𝐚𝐭 𝐈𝐝 🥀 : `{message.chat.id}` \n\n𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 [𝐁𝐢𝐤𝐚𝐬𝐡 𝐆𝐚𝐝𝐠𝐞𝐭𝐬 𝐓𝐞𝐜𝐡](https://t.me/BikashGadgetsTech)"
         )
     else:
         message.reply(
