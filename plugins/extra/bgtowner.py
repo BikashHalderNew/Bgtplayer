d66ac4396ef3afa570d85c7c872172d874fade27
Bikash Halder <115603304+BikashHalderNew@users.noreply.github.com>
2023-08-04 21:37:02 +0530

    Create bgtowner.py

---
+++ plugins/extra/bgtowner.py
@@ -0,0 +1,22 @@
+## Bikash Halder & Aditya Halder
+
+
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from Bikash.config import LOG_GROUP_ID
+from Bikash import app
+
+
+async def new_message(chat_id: int, message: str):
+    await app.send_message(chat_id=chat_id, text=message)
+
+
+@app.on_message(filters.new_chat_members)
+async def on_new_chat_members(client: Client, message: Message):
+    if (await client.get_me()).id in [user.id for user in message.new_chat_members]:
+        added_by = message.from_user.mention if message.from_user else "Bgt User"
+        title = message.chat.title
+        chat_id = message.chat.id
+        bgt = f"🥀 𝐁𝐠𝐭 𝐌𝐮𝐬𝐢𝐜 𝐀𝐝𝐝𝐞𝐝 𝐀 𝐍𝐞𝐰 𝐆𝐫𝐨𝐮𝐩 🥀\n\n🥀 𝐆𝐫𝐨𝐮𝐩 𝐈𝐝 : {chat_id} 🌴\n🥀 𝐆𝐫𝐨𝐮𝐩 𝐍𝐚𝐦𝐞 : {title} 🌺\n🥀 𝐀𝐝𝐝𝐞𝐝 𝐁𝐲 : {added_by} 🌱"
+        await new_message(LOG_GROUP_ID, bgt)
