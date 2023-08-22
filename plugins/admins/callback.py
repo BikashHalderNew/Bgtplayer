dabbf9d0bee6cf5ec0bd1ca5bf830842eaf0e645
Lehar MuSiC <103201972+Leharmusicbot@users.noreply.github.com>
2023-08-22 14:27:11 +0530

    Update callback.py

--- plugins/admins/callback.py
+++ plugins/admins/callback.py
@@ -83,7 +83,7 @@ async def unban_assistant_(_, CallbackQuery):
             await app.unban_chat_member(int(chat_id), int(user_id))
         except:
             return await CallbackQuery.answer(
-                " 𝐅𝐚𝐢𝐥𝐞𝐝 𝐓𝐨 𝐔𝐧𝐛𝐚𝐧 𝐁𝐠𝐭 𝐀𝐬𝐬𝐢𝐬𝐭𝐚𝐧𝐭.",
+                " 𝐅𝐚𝐢𝐥𝐞𝐝 𝐓𝐨 𝐔𝐧𝐛𝐚𝐧 𝐊𝚰𝐓𝐓𝐔 𝐀𝐬𝐬𝐢𝐬𝐭𝐚𝐧𝐭.",
                 show_alert=True,
             )
         return await CallbackQuery.edit_message_text(
