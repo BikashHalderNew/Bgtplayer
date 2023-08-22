f319380c150d273991b5378069c8630c240ff9fa
Lehar MuSiC <103201972+Leharmusicbot@users.noreply.github.com>
2023-08-22 15:16:15 +0530

    Update help.py

--- Bikash/utils/inline/help.py
+++ Bikash/utils/inline/help.py
@@ -13,11 +13,11 @@ def help_pannel(_, START: Union[bool, int] = None):
         InlineKeyboardButton(
             # text=_["BACK_BUTTON"],
             text="🏠 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 🏠",
-            url=f"https://t.me/BikashGadgetsTech",
+            url=f"https://t.me/hindi_chattings_zone",
         ),
         InlineKeyboardButton(
             text="𝐂𝐫𝐞𝐚𝐭𝐨𝐫",
-            url=f"t.me/BikashHalder",
+            url=f"t.me/Mr_pr0feSser",
         ),
         InlineKeyboardButton(
             text="𝐂𝐥𝐨𝐬𝐞", callback_data=f"close"
