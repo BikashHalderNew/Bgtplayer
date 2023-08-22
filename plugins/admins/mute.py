d5d3f183b9cd490c4414071603dd6d46663a0edb
Lehar MuSiC <103201972+Leharmusicbot@users.noreply.github.com>
2023-08-22 14:23:52 +0530

    Update mute.py

--- plugins/admins/mute.py
+++ plugins/admins/mute.py
@@ -24,7 +24,7 @@ async def mute_admin(cli, message: Message, _, chat_id):
     if not len(message.command) == 1 or message.reply_to_message:
         return await message.reply_text("**❌ 𝐄𝐫𝐫𝐨𝐫, 𝐖𝐫𝐨𝐧𝐠 𝐔𝐬𝐚𝐠𝐞 𝐎𝐟 𝐂𝐨𝐦𝐦𝐚𝐧𝐝❗...**")
     if await is_muted(chat_id):
-        return await message.reply_text("**🔇 𝐁𝐠𝐭 𝐌𝐮𝐬𝐢𝐜 𝐀𝐥𝐫𝐞𝐚𝐝𝐲 𝐌𝐮𝐭𝐞𝐝 🌷 ...**")
+        return await message.reply_text("**🔇 𝐊𝚰𝐓𝐓𝐔 𝐌𝐮𝐬𝐢𝐜 𝐀𝐥𝐫𝐞𝐚𝐝𝐲 𝐌𝐮𝐭𝐞𝐝 🌷 ...**")
     await mute_on(chat_id)
     await Bikashh.mute_stream(chat_id)
     await message.reply_text("**🔇 𝐌𝐮𝐭𝐞𝐝 🌷 ...**\n\n⎿𝐑𝐞𝐪𝐮𝐞𝐬𝐭𝐞𝐝 𝐁𝐲 > {}")
