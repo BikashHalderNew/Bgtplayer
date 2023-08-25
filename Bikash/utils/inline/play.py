3c58f08ab322b96003c69b4121965c2a66dfc35e
Lehar MuSiC <103201972+Leharmusicbot@users.noreply.github.com>
2023-08-25 16:34:22 +0530

    Update play.py

--- Bikash/utils/inline/play.py
+++ Bikash/utils/inline/play.py
@@ -14,25 +14,25 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     bikash = math.floor(percentage)
     if 0 < bikash <= 10:
-        bar = "bgt════════"
+        bar = "◉—————————"
     elif 10 < bikash < 20:
-        bar = "═bgt═══════"
+        bar = "—◉————————"
     elif 20 <= bikash < 30:
-        bar = "══bgt══════"
+        bar = "——◉———————"
     elif 30 <= bikash < 40:
-        bar = "═══bgt═════"
+        bar = "———◉——————"
     elif 40 <= bikash < 50:
-        bar = "════bgt════"
+        bar = "————◉—————"
     elif 50 <= bikash < 60:
-        bar = "═════bgt═══"
+        bar = "—————◉————"
     elif 60 <= bikash < 70:
-        bar = "══════bgt══"
+        bar = "——————◉———"
     elif 70 <= bikash < 80:
-        bar = "═══════bgt═"
+        bar = "———————◉——"
     elif 80 <= bikash < 95:
-        bar = "════════bgt"
+        bar = "————————◉—"
     else:
-        bar = "════════BGT"
+        bar = "—————————◉"
 
     buttons = [
         [
@@ -91,25 +91,25 @@ def telegram_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     bikash = math.floor(percentage)
     if 0 < bikash <= 10:
-        bar = "bgt════════"
+        bar = "◉—————————"
     elif 10 < bikash < 20:
-        bar = "═bgt═══════"
+        bar = "—◉————————"
     elif 20 <= bikash < 30:
-        bar = "══bgt══════"
+        bar = "——◉———————"
     elif 30 <= bikash < 40:
-        bar = "═══bgt═════"
+        bar = "———◉——————"
     elif 40 <= bikash < 50:
-        bar = "════bgt════"
+        bar = "————◉—————"
     elif 50 <= bikash < 60:
-        bar = "═════bgt═══"
+        bar = "—————◉————"
     elif 60 <= bikash < 70:
-        bar = "══════bgt══"
+        bar = "——————◉———"
     elif 70 <= bikash < 80:
-        bar = "═══════bgt═"
+        bar = "———————◉——"
     elif 80 <= bikash < 95:
-        bar = "════════bgt"
+        bar = "————————◉—"
     else:
-        bar = "════════BGT"
+        bar = "—————————◉"
 
     buttons = [
         [
