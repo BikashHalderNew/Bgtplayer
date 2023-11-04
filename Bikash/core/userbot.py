11b78058d0a6e5b351d80adc088e01e94852ae9a
it-xd <149780872+it-xd@users.noreply.github.com>
2023-11-04 21:58:09 +0530

    Update userbot.py

--- Bikash/core/userbot.py
+++ Bikash/core/userbot.py
@@ -13,33 +13,38 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
+            name="Userbot1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
-            session_name=str(config.STRING1),
+            session_string=str(config.STRING1),
             no_updates=True,
         )
         self.two = Client(
+            name="Userbot2",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
-            session_name=str(config.STRING2),
+            session_string=str(config.STRING2),
             no_updates=True,
         )
         self.three = Client(
+            name="Userbot3",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
-            session_name=str(config.STRING3),
+            session_string=str(config.STRING3),
             no_updates=True,
         )
         self.four = Client(
+            name="Userbot4",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
-            session_name=str(config.STRING4),
+            session_string=str(config.STRING4),
             no_updates=True,
         )
         self.five = Client(
+            name="Userbot5",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
-            session_name=str(config.STRING5),
+            session_string=str(config.STRING5),
             no_updates=True,
         )
 
