baccf6f12ac87942505d338432375b3ac1124c03
it-xd <149780872+it-xd@users.noreply.github.com>
2023-11-04 22:04:34 +0530

    Update shuffle.py

--- plugins/admins/shuffle.py
+++ plugins/admins/shuffle.py
@@ -22,7 +22,6 @@ SHUFFLE_COMMAND = get_command("SHUFFLE_COMMAND")
 @app.on_message(
     filters.command(SHUFFLE_COMMAND)
     & filters.group
-    & ~filters.edited
     & ~BANNED_USERS
 )
 @AdminRightsCheck
