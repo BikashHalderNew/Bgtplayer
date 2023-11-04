d78bbfc1fbf416823416cc2009b3a6f026e70293
it-xd <149780872+it-xd@users.noreply.github.com>
2023-11-04 22:07:50 +0530

    Update start.py

--- plugins/bot/start.py
+++ plugins/bot/start.py
@@ -38,7 +38,6 @@ loop = asyncio.get_running_loop()
 @app.on_message(
     filters.command(get_command("START_COMMAND"))
     & filters.private
-    & ~filters.edited
     & ~BANNED_USERS
 )
 @LanguageStart
