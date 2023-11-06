f530c89ac584b32fd2fd21c422110ae6344755f5
crowereck <149960260+crowereck@users.noreply.github.com>
2023-11-07 01:38:50 +0530

    Update start.py

--- plugins/bot/start.py
+++ plugins/bot/start.py
@@ -1,7 +1,12 @@
-# Powered By @BikashHalder & @AdityaHalder 
-# Join @BikashGadgetsTech For More Updates
-# Join @AdityaCheats For Hacks
-# Join Our Chats @Bgt_Chat & @Adityadiscus 
+#
+# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+
 
 import asyncio
 import time
@@ -47,15 +52,14 @@ async def start_comm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            return await message.reply_photo(
-                       photo=config.START_IMG_URL,
-                       caption=_["help_1"], reply_markup=keyboard
+            return await message.reply_text(
+                _["help_1"], reply_markup=keyboard
             )
         if name[0:4] == "song":
             return await message.reply_text(_["song_2"])
         if name[0:3] == "sta":
             m = await message.reply_text(
-                f"🥱 𝐆𝐞𝐭𝐭𝐢𝐧𝐠 𝐘𝐨𝐮𝐫 𝐏𝐞𝐫𝐬𝐨𝐧𝐚𝐥 𝐒𝐭𝐚𝐭𝐬 𝐅𝐫𝐨𝐦 {config.MUSIC_BOT_NAME} 𝐒𝐞𝐫𝐯𝐞𝐫."
+                "🔎 Fetching your personal stats.!"
             )
             stats = await get_userss(message.from_user.id)
             tot = len(stats)
@@ -91,7 +95,7 @@ async def start_comm(client, message: Message, _):
                     details = stats.get(vidid)
                     title = (details["title"][:35]).title()
                     if vidid == "telegram":
-                        msg += f"🔗[𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐌𝐞𝐝𝐢𝐚]({config.SUPPORT_GROUP}) ** 𝐏𝐥𝐚𝐲𝐞𝐝 {count} 𝐓𝐢𝐦𝐞𝐬**\n\n"
+                        msg += f"🔗[Telegram Files and Audios](https://t.me/telegram) ** played {count} times**\n\n"
                     else:
                         msg += f"🔗 [{title}](https://www.youtube.com/watch?v={vidid}) ** played {count} times**\n\n"
                 msg = _["ustats_2"].format(tot, tota, limit) + msg
@@ -115,7 +119,7 @@ async def start_comm(client, message: Message, _):
                 sender_name = message.from_user.first_name
                 return await app.send_message(
                     config.LOG_GROUP_ID,
-                    f"{message.from_user.mention} 𝐉𝐮𝐬𝐭 𝐒𝐭𝐚𝐫𝐭𝐞𝐝 𝐓𝐡𝐞 𝐁𝐨𝐭 𝐓𝐨 𝐂𝐡𝐞𝐜𝐤 <code>𝐒𝐮𝐝𝐨𝐥𝐢𝐬𝐭</code>\n\n**𝐔𝐬𝐞𝐫 𝐈𝐝:** {sender_id}\n**𝐔𝐬𝐞𝐫𝐧𝐚𝐦𝐞:** {sender_name}",
+                    f"{message.from_user.mention} has just started bot to check <code>SUDOLIST</code>\n\n**USER ID:** {sender_id}\n**USER NAME:** {sender_name}",
                 )
             return
         if name[0:3] == "lyr":
@@ -126,22 +130,12 @@ async def start_comm(client, message: Message, _):
                 return await Telegram.send_split_text(message, lyrics)
             else:
                 return await message.reply_text(
-                    "𝐅𝐚𝐢𝐥𝐞𝐝 𝐓𝐨 𝐆𝐞𝐭 𝐋𝐲𝐫𝐢𝐜𝐬."
+                    "Failed to get lyrics."
                 )
         if name[0:3] == "del":
             await del_plist_msg(client=client, message=message, _=_)
-        if name == "verify":
-            await message.reply_text(f"𝐇𝐞𝐲 {message.from_user.first_name},\n𝐓𝐡𝐚𝐧𝐤𝐬 𝐅𝐨𝐫 𝐕𝐞𝐫𝐢𝐟𝐲𝐢𝐧𝐠 𝐘𝐨𝐮𝐫𝐬𝐞𝐥𝐟 𝐈𝐧 {config.MUSIC_BOT_NAME}, 𝐍𝐨𝐰 𝐘𝐨𝐮 𝐂𝐚𝐧 𝐆𝐨 𝐁𝐚𝐜𝐤 & 𝐒𝐭𝐚𝐫𝐭 𝐔𝐬𝐢𝐧𝐠 𝐌𝐞.")
-            if await is_on_off(config.LOG):
-                sender_id = message.from_user.id
-                sender_name = message.from_user.first_name
-                return await app.send_message(
-                    config.LOG_GROUP_ID,
-                    f"{message.from_user.mention} 𝐉𝐮𝐬𝐭 𝐒𝐭𝐚𝐫𝐭𝐞𝐝 𝐓𝐡𝐞 𝐁𝐨𝐭 𝐓𝐨 <code> 𝐕𝐞𝐫𝐢𝐟𝐲 𝐇𝐢𝐦𝐬𝐞𝐥𝐟</code>\n\n**𝐔𝐬𝐞𝐫 𝐈𝐝:** {sender_id}\n**𝐔𝐬𝐞𝐫𝐧𝐚𝐦𝐞:** {sender_name}",
-                )
-            return
         if name[0:3] == "inf":
-            m = await message.reply_text("🔎")
+            m = await message.reply_text("🔎 Fetching Info!")
             query = (str(name)).replace("info_", "", 1)
             query = f"https://www.youtube.com/watch?v={query}"
             results = VideosSearch(query, limit=1)
@@ -157,26 +151,26 @@ async def start_comm(client, message: Message, _):
                 link = result["link"]
                 published = result["publishedTime"]
             searched_text = f"""
-🌺**𝐓𝐫𝐚𝐜𝐤 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧 **🌺
+<b>🔍Video Track Information<b>
 
-🥀 **𝐓𝐢𝐭𝐥𝐞:** {title}
+❇️<b>Title:<b> {title}
 
-⏳ **𝐃𝐮𝐫𝐚𝐭𝐢𝐨𝐧:** {duration} ᴍɪɴᴜᴛᴇs
-👀 **𝐕𝐢𝐞𝐰𝐬:** `{views}`
-⏰ **𝐏𝐮𝐛𝐥𝐢𝐬𝐡𝐞𝐝 𝐎𝐧:** {published}
-🎥 **𝐂𝐡𝐚𝐧𝐧𝐞𝐥:** {channel}
-📎 **𝐂𝐡𝐚𝐧𝐧𝐞𝐥 𝐋𝐢𝐧𝐤:** [𝐕𝐢𝐬𝐢𝐭 𝐂𝐡𝐚𝐧𝐧𝐞𝐥]({channellink})
-🔗 **𝐋𝐢𝐧𝐤:** [𝐖𝐚𝐭𝐜𝐡 𝐎𝐧 𝐘𝐨𝐮𝐭𝐮𝐛𝐞]({link})
+⏳<b>Duration:<b> {duration} Mins
+👀<b>Views:<b> {views}
+⏰<b>Published Time:<b> {published}
+🎥<b>Channel Name:<b> {channel}
+📎<b>Channel Link:<b> [Visit From Here]({channellink})
+🔗<b>Video Link:<b> [Link]({link})
 
-💖 𝐒𝐞𝐚𝐫𝐜𝐡 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 {config.MUSIC_BOT_NAME}"""
+⚡️ Searched Powered By {config.MUSIC_BOT_NAME}"""
             key = InlineKeyboardMarkup(
                 [
                     [
                         InlineKeyboardButton(
-                            text="• 𝐘𝐨𝐮𝐭𝐮𝐛𝐞 •", url=f"{link}"
+                            text="🎥 Watch ", url=f"{link}"
                         ),
                         InlineKeyboardButton(
-                            text="🌿 𝐆𝐫𝐨𝐮𝐩 🌿", url=config.SUPPORT_GROUP
+                            text="🔄 Close", callback_data="close"
                         ),
                     ],
                 ]
@@ -194,7 +188,7 @@ async def start_comm(client, message: Message, _):
                 sender_name = message.from_user.first_name
                 return await app.send_message(
                     config.LOG_GROUP_ID,
-                    f"{message.from_user.mention} 𝐉𝐮𝐬𝐭 𝐒𝐭𝐚𝐫𝐭𝐞𝐝 𝐁𝐨𝐭 𝐓𝐨 𝐂𝐡𝐞𝐜𝐤 <code> 𝐓𝐫𝐚𝐜𝐤 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧</code>\n\n**𝐔𝐬𝐞𝐫 𝐈𝐝:** {sender_id}\n**𝐔𝐬𝐞𝐫𝐧𝐚𝐦𝐞:** {sender_name}",
+                    f"{message.from_user.mention} has just started bot to check <code>VIDEO INFORMATION</code>\n\n**USER ID:** {sender_id}\n**USER NAME:** {sender_name}",
                 )
     else:
         try:
@@ -227,7 +221,7 @@ async def start_comm(client, message: Message, _):
             sender_name = message.from_user.first_name
             return await app.send_message(
                 config.LOG_GROUP_ID,
-                f"{message.from_user.mention} 𝐉𝐮𝐬𝐭 𝐒𝐭𝐚𝐫𝐭𝐞𝐝 𝐘𝐨𝐮𝐫 𝐁𝐨𝐭.\n\n**𝐔𝐬𝐞𝐫 𝐈𝐝:** {sender_id}\n**𝐔𝐬𝐞𝐫𝐧𝐚𝐦𝐞:** {sender_name}",
+                f"{message.from_user.mention} has just started Bot.\n\n**USER ID:** {sender_id}\n**USER NAME:** {sender_name}",
             )
 
 
@@ -238,11 +232,9 @@ async def start_comm(client, message: Message, _):
 )
 @LanguageStart
 async def testbot(client, message: Message, _):
-    OWNER = OWNER_ID[0]
-    out = start_pannel(_, app.username, OWNER)
-    return await message.reply_photo(
-               photo=config.START_IMG_URL,
-               caption=_["start_1"].format(
+    out = start_pannel(_)
+    return await message.reply_text(
+        _["start_1"].format(
             message.chat.title, config.MUSIC_BOT_NAME
         ),
         reply_markup=InlineKeyboardMarkup(out),
@@ -258,7 +250,7 @@ async def welcome(client, message: Message):
     if config.PRIVATE_BOT_MODE == str(True):
         if not await is_served_private_chat(message.chat.id):
             await message.reply_text(
-                "**𝐏𝐫𝐢𝐯𝐚𝐭𝐞 𝐌𝐮𝐬𝐢𝐜 𝐁𝐨𝐭**\n\n𝐎𝐧𝐥𝐲 𝐅𝐨𝐫 𝐓𝐡𝐞 𝐂𝐡𝐚𝐭𝐬 𝐀𝐮𝐭𝐡𝐨𝐫𝐢𝐞𝐬 𝐁𝐲 𝐌𝐲 𝐎𝐰𝐧𝐞𝐫, 𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐈𝐧 𝐌𝐲 𝐎𝐰𝐧𝐞𝐫 𝐏𝐦 𝐓𝐨 𝐀𝐮𝐭𝐡𝐨𝐫𝐢𝐞𝐬 𝐘𝐨𝐮𝐫 𝐂𝐡𝐚𝐭 & 𝐈𝐟 𝐘𝐨𝐮 𝐃𝐨𝐧'𝐭 𝐖𝐚𝐧𝐭 𝐓𝐨 𝐃𝐨 𝐓𝐡𝐞𝐍 𝐋𝐞𝐚𝐯𝐞 𝐂𝐡𝐚𝐭."
+                "**Private Music Bot**\n\nOnly for authorized chats from the owner. Ask my owner to allow your chat first."
             )
             return await app.leave_chat(message.chat.id)
     else:
@@ -280,11 +272,9 @@ async def welcome(client, message: Message):
                     )
                     return await app.leave_chat(chat_id)
                 userbot = await get_assistant(message.chat.id)
-                OWNER = OWNER_ID[0]
-                out = start_pannel(_, app.username, OWNER)
-                await message.reply_photo(
-                    photo=config.START_IMG_URL,
-                    caption=_["start_3"].format(
+                out = start_pannel(_)
+                await message.reply_text(
+                    _["start_3"].format(
                         config.MUSIC_BOT_NAME,
                         userbot.username,
                         userbot.id,
@@ -306,10 +296,3 @@ async def welcome(client, message: Message):
             return
         except:
             return
-
-
-
-# Powered By @BikashHalder & @AdityaHalder 
-# Join @BikashGadgetsTech For More Updates
-# Join @AdityaCheats For Hacks
-# Join Our Chats @Bgt_Chat & @Adityadiscus 
