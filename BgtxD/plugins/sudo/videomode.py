5dc57b95e7e4a39c716a87883cef341558d17f36
James Web <149960260+crowereck@users.noreply.github.com>
2023-11-11 01:31:13 +0530

    Create videomode.py

---
+++ BgtxD/plugins/sudo/videomode.py
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2023-2024 by YukkiOwner@Github, < https://github.com/YukkiOwner >.
+#
+# This file is part of < https://github.com/YukkiOwner/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/YukkiOwner/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from BgtxD import config
+from strings import get_command
+from BgtxD import app
+from BgtxD.misc import SUDOERS
+from BgtxD.utils.database import add_off, add_on
+from BgtxD.utils.decorators.language import language
+
+# Commands
+VIDEOMODE_COMMAND = get_command("VIDEOMODE_COMMAND")
+
+
+@app.on_message(filters.command(VIDEOMODE_COMMAND) & SUDOERS)
+@language
+async def videoloaymode(client, message: Message, _):
+    usage = _["vidmode_1"]
+    if len(message.command) != 2:
+        return await message.reply_text(usage)
+    state = message.text.split(None, 1)[1].strip()
+    state = state.lower()
+    if state == "download":
+        await add_on(config.YTDOWNLOADER)
+        await message.reply_text(_["vidmode_2"])
+    elif state == "m3u8":
+        await add_off(config.YTDOWNLOADER)
+        await message.reply_text(_["vidmode_3"])
+    else:
+        await message.reply_text(usage)
