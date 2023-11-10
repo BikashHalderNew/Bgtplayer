9219cc6a9a406f431776c956c10342e5817bd904
James Web <149960260+crowereck@users.noreply.github.com>
2023-11-10 23:45:27 +0530

    Create dir.py

---
+++ BgtxD/core/dir.py
@@ -0,0 +1,31 @@
+
+import os
+import sys
+from os import listdir, mkdir
+
+from ..logging import LOGGER
+
+
+def dirr():
+    if "resources" not in listdir():
+        LOGGER(__name__).warning(
+            f"resources Folder not Found. Please clone repository again."
+        )
+        sys.exit()
+    for file in os.listdir():
+        if file.endswith(".jpg"):
+            os.remove(file)
+    for file in os.listdir():
+        if file.endswith(".jpeg"):
+            os.remove(file)
+    for file in os.listdir():
+        if file.endswith(".session"):
+            os.remove(file)
+    for file in os.listdir():
+        if file.endswith(".session-journal"):
+            os.remove(file)
+    if "downloads" not in listdir():
+        mkdir("downloads")
+    if "cache" not in listdir():
+        mkdir("cache")
+    LOGGER(__name__).info("Directories Updated.")
