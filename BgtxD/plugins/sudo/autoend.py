52c8e105731a525a431638ed0f0f764a5c289daa
James Web <149960260+crowereck@users.noreply.github.com>
2023-11-11 01:18:23 +0530

    Create autoend.py

---
+++ BgtxD/plugins/sudo/autoend.py
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2023-2024 by YukkiOwner@Github, < https://github.com/YukkiOwner >.
+#
+# This file is part of < https://github.com/YukkiOwner/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/YukkiOwner/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+from pyrogram import filters
+
+from BgtxD import config
+from strings import get_command
+from YukkiMusic import app
+from BgtxD.misc import SUDOERS
+from BgtxD.utils.database import autoend_off, autoend_on
+from BgtxD.utils.decorators.language import language
+
+# Commands
+AUTOEND_COMMAND = get_command("AUTOEND_COMMAND")
+
+
+@app.on_message(filters.command(AUTOEND_COMMAND) & SUDOERS)
+async def auto_end_stream(client, message):
+    usage = "**Usage:**\n\n/autoend [enable|disable]"
+    if len(message.command) != 2:
+        return await message.reply_text(usage)
+    state = message.text.split(None, 1)[1].strip()
+    state = state.lower()
+    if state == "enable":
+        await autoend_on()
+        await message.reply_text(
+            "Auto End Stream Enabled.\n\nBot will leave voice chat automatically after 3 mins if no one is listening with a warning message.."
+        )
+    elif state == "disable":
+        await autoend_off()
+        await message.reply_text("Auto End Stream Disabled.")
+    else:
+        await message.reply_text(usage)
