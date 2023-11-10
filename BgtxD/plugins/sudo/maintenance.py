76a975f6a645442731c94dc51df35b6e9258bda5
James Web <149960260+crowereck@users.noreply.github.com>
2023-11-11 01:25:21 +0530

    Create maintenance.py

---
+++ BgtxD/plugins/sudo/maintenance.py
@@ -0,0 +1,56 @@
+#
+# Copyright (C) 2023-2024 by YukkiOwner@Github, < https://github.com/YukkiOwner >.
+#
+# This file is part of < https://github.com/YukkiOwner/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/YukkiOwner/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from strings import get_command, get_string
+from BgtxD import app
+from BgtxD.misc import SUDOERS
+from BgtxD.utils.database import (get_lang, is_maintenance,
+                                       maintenance_off,
+                                       maintenance_on)
+from BgtxD.utils.decorators.language import language
+
+# Commands
+MAINTENANCE_COMMAND = get_command("MAINTENANCE_COMMAND")
+
+
+@app.on_message(filters.command(MAINTENANCE_COMMAND) & SUDOERS)
+async def maintenance(client, message: Message):
+    try:
+        language = await get_lang(message.chat.id)
+        _ = get_string(language)
+    except:
+        _ = get_string("en")
+    usage = _["maint_1"]
+    if len(message.command) != 2:
+        return await message.reply_text(usage)
+    message.chat.id
+    state = message.text.split(None, 1)[1].strip()
+    state = state.lower()
+    if state == "enable":
+        if await is_maintenance() is False:
+            await message.reply_text(
+                "Maintenance mode is already enabled"
+            )
+        else:
+            await maintenance_on()
+            await message.reply_text(_["maint_2"])
+    elif state == "disable":
+        if await is_maintenance() is False:
+            await maintenance_off()
+            await message.reply_text(_["maint_3"])
+        else:
+            await message.reply_text(
+                "Maintenance mode is already disabled"
+            )
+    else:
+        await message.reply_text(usage)
