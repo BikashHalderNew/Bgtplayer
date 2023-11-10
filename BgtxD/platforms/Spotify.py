ca3abd718133af91359c78d5f66d30401bc33bb5
James Web <149960260+crowereck@users.noreply.github.com>
2023-11-11 02:57:45 +0530

    Update Spotify.py

--- BgtxD/platforms/Spotify.py
+++ BgtxD/platforms/Spotify.py
@@ -14,7 +14,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from youtubesearchpython.__future__ import VideosSearch
 
-import config
+from BgtxD import config
 
 
 class SpotifyAPI:
