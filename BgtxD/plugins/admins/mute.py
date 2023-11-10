b4e31324966ed8c293a1953d03a192daed7dbaef
James Web <149960260+crowereck@users.noreply.github.com>
2023-11-11 00:07:43 +0530

    Create mute.py

---
+++ BgtxD/plugins/admins/mute.py
@@ -0,0 +1,32 @@
+
+
+from pyrogram import filters
+from pyrogram.types import Message
+from BgtxD import config
+from config import BANNED_USERS
+from strings import get_command
+from BgtxD import app
+from BgtxD.core.call import Bikash
+from BgtxD.utils.database import is_muted, mute_on
+from BgtxD.utils.decorators import AdminRightsCheck
+
+# Commands
+MUTE_COMMAND = get_command("MUTE_COMMAND")
+
+
+@app.on_message(
+    filters.command(MUTE_COMMAND)
+    & filters.group
+    & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def mute_admin(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1 or message.reply_to_message:
+        return await message.reply_text(_["general_2"])
+    if await is_muted(chat_id):
+        return await message.reply_text(_["admin_5"])
+    await mute_on(chat_id)
+    await Yukki.mute_stream(chat_id)
+    await message.reply_text(
+        _["admin_6"].format(message.from_user.mention)
+    )
