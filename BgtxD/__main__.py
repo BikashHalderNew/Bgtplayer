19dbd8ca91a6a5173d09e497c75f4069d7134999
James Web <149960260+crowereck@users.noreply.github.com>
2023-11-10 22:39:32 +0530

    Update __main__.py

--- BgtxD/__main__.py
+++ BgtxD/__main__.py
@@ -1,1 +1,72 @@
+import asyncio
+import importlib
+import sys
 
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
+
+from BgtxD import config
+from config import BANNED_USERS
+from  import LOGGER, app, userbot
+from BgtxD.core.call import Bgt
+from BgtxD.plugins import ALL_MODULES
+from BgtxD.utils.database import get_banned_users, get_gbanned
+
+loop = asyncio.get_event_loop()
+
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER("BgtxD").error(
+            "No Assistant Clients Vars Defined!.. Exiting Process."
+        )
+        return
+    if (
+        not config.SPOTIFY_CLIENT_ID
+        and not config.SPOTIFY_CLIENT_SECRET
+    ):
+        LOGGER("BgtxD").warning(
+            "No Spotify Vars defined. Your bot won't be able to play spotify queries."
+        )
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("YukkiMusic.plugins" + all_module)
+    LOGGER("BgtxD.plugins").info(
+        "Successfully Imported Modules "
+    )
+    await userbot.start()
+    await Bgt.start()
+    try:
+        await Bgt.stream_call(
+            "http://docs.evostream.com/sample_content/assets/sintel1m720p.mp4"
+        )
+    except NoActiveGroupCall:
+        LOGGER("BgtxD").error(
+            "[ERROR] - \n\nPlease turn on your Logger Group's Voice Call. Make sure you never close/end voice call in your log group"
+        )
+        sys.exit()
+    except:
+        pass
+    await Bgt.decorators()
+    LOGGER("BgtxD").info("Yukki Music Bot Started Successfully")
+    await idle()
+
+
+if __name__ == "__main__":
+    loop.run_until_complete(init())
+    LOGGER("BgtxD").info("Stopping Yukki Music Bot! GoodBye")
