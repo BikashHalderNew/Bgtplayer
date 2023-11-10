47527637139202e12c3c67a7cc95583938e18393
James Web <149960260+crowereck@users.noreply.github.com>
2023-11-11 00:09:30 +0530

    Create pause.py

---
+++ BgtxD/plugins/admins/pause.py
@@ -0,0 +1,32 @@
+
+
+from pyrogram import filters
+from pyrogram.types import Message
+from BgtxD import config
+from config import BANNED_USERS
+from strings import get_command
+from BgtxD import app
+from BgtxD.core.call import Bikash
+from BgtxD.utils.database import is_music_playing, music_off
+from BgtxD.utils.decorators import AdminRightsCheck
+
+# Commands
+PAUSE_COMMAND = get_command("PAUSE_COMMAND")
+
+
+@app.on_message(
+    filters.command(PAUSE_COMMAND)
+    & filters.group
+    & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def pause_admin(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return await message.reply_text(_["general_2"])
+    if not await is_music_playing(chat_id):
+        return await message.reply_text(_["admin_1"])
+    await music_off(chat_id)
+    await Bikash.pause_stream(chat_id)
+    await message.reply_text(
+        _["admin_2"].format(message.from_user.mention)
+    )
