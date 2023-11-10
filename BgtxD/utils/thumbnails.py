e5f44148e6e1f2bedc551c755aadf83208265f5b
James Web <149960260+crowereck@users.noreply.github.com>
2023-11-11 03:32:43 +0530

    Update thumbnails.py

--- BgtxD/utils/thumbnails.py
+++ BgtxD/utils/thumbnails.py
@@ -7,8 +7,7 @@ import aiohttp
 from PIL import (Image, ImageDraw, ImageEnhance, ImageFilter,
                  ImageFont, ImageOps)
 from youtubesearchpython.__future__ import VideosSearch
-from BgtxD import config
-from config import MUSIC_BOT_NAME, YOUTUBE_IMG_URL
+from BgtxD.config import MUSIC_BOT_NAME, YOUTUBE_IMG_URL
 
 
 def changeImageSize(maxWidth, maxHeight, image):
