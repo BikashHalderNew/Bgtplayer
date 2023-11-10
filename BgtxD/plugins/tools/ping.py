1c07569a160a8b89ba1dec8b4d07c42da231d9db
James Web <149960260+crowereck@users.noreply.github.com>
2023-11-11 01:15:43 +0530

    Create ping.py

---
+++ BgtxD/plugins/tools/ping.py
@@ -0,0 +1,46 @@
+#
+# Copyright (C) 2023-2024 by YukkiOwner@Github, < https://github.com/YukkiOwner >.
+#
+# This file is part of < https://github.com/YukkiOwner/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/YukkiOwner/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+from datetime import datetime
+
+from pyrogram import filters
+from pyrogram.types import Message
+from BgtxD import config 
+from config import BANNED_USERS, MUSIC_BOT_NAME, PING_IMG_URL
+from strings import get_command
+from BgtxD import app
+from BgtxD.core.call import Bikash
+from BgtxD.utils import bot_sys_stats
+from BgtxD.utils.decorators.language import language
+
+### Commands
+PING_COMMAND = get_command("PING_COMMAND")
+
+
+@app.on_message(
+    filters.command(PING_COMMAND)
+    & filters.group
+    & ~BANNED_USERS
+)
+@language
+async def ping_com(client, message: Message, _):
+    response = await message.reply_photo(
+        photo=PING_IMG_URL,
+        caption=_["ping_1"],
+    )
+    start = datetime.now()
+    pytgping = await Bikash.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+    await response.edit_text(
+        _["ping_2"].format(
+            MUSIC_BOT_NAME, resp, UP, DISK, CPU, RAM, pytgping
+        )
+    )
