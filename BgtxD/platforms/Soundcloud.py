769097e12be323351ab2307a549f407442b3daf2
James Web <149960260+crowereck@users.noreply.github.com>
2023-11-10 23:30:11 +0530

    Update Soundcloud.py

--- BgtxD/platforms/Soundcloud.py
+++ BgtxD/platforms/Soundcloud.py
@@ -13,7 +13,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from YukkiMusic.utils.formatters import seconds_to_min
+from BgtxD.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
