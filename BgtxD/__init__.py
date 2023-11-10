3db8b0f15a35131d369f5499e2df0fca2bd94a44
James Web <149960260+crowereck@users.noreply.github.com>
2023-11-10 22:35:30 +0530

    Update __init__.py

--- BgtxD/__init__.py
+++ BgtxD/__init__.py
@@ -1,1 +1,38 @@
+from BgtxD.core.bot import BgtBot
+from BgtxD.core.dir import dirr
+from BgtxD.core.git import git
+from BgtxD.core.userbot import Userbot
+from BgtxD.misc import dbb, heroku, sudo
 
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = YukkiBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
