35d2d6b3d1500804fe23ecd3fe7b5a521b970702
James Web <149960260+crowereck@users.noreply.github.com>
2023-11-11 04:17:42 +0530

    Update assistantdatabase.py

--- BgtxD/utils/database/assistantdatabase.py
+++ BgtxD/utils/database/assistantdatabase.py
@@ -32,7 +32,7 @@ async def get_client(assistant: int):
 
 
 async def set_assistant(chat_id):
-    from YukkiMusic.core.userbot import assistants
+    from BgtxD.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -46,7 +46,7 @@ async def set_assistant(chat_id):
 
 
 async def get_assistant(chat_id: int) -> str:
-    from YukkiMusic.core.userbot import assistants
+    from BgtxD.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
@@ -73,7 +73,7 @@ async def get_assistant(chat_id: int) -> str:
 
 
 async def set_calls_assistant(chat_id):
-    from YukkiMusic.core.userbot import assistants
+    from BgtxD.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -86,7 +86,7 @@ async def set_calls_assistant(chat_id):
 
 
 async def group_assistant(self, chat_id: int) -> int:
-    from YukkiMusic.core.userbot import assistants
+    from BgtxD.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
