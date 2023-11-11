1ea51fa842bc31dd59e9c552b4900ee9c7127eff
James Web <149960260+crowereck@users.noreply.github.com>
2023-11-11 06:20:38 +0530

    Update stats.py

--- BgtxD/plugins/tools/stats.py
+++ BgtxD/plugins/tools/stats.py
@@ -11,7 +11,7 @@
 import asyncio
 import platform
 from sys import version as pyver
-
+from BgtxD import config
 import psutil
 from pyrogram import __version__ as pyrover
 from pyrogram import filters
