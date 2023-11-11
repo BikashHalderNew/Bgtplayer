0fd557a8dd0262b630cca29c4113aa20da79e527
James Web <149960260+crowereck@users.noreply.github.com>
2023-11-11 06:36:31 +0530

    Update videolimit.py

--- BgtxD/plugins/sudo/videolimit.py
+++ BgtxD/plugins/sudo/videolimit.py
@@ -1,13 +1,4 @@
-#
-# Copyright (C) 2023-2024 by YukkiOwner@Github, < https://github.com/YukkiOwner >.
-#
-# This file is part of < https://github.com/YukkiOwner/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/YukkiOwner/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
+from BgtxD import config
 from pyrogram import filters
 from pyrogram.types import Message
 
