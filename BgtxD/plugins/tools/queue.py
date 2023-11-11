414b79b294b5f6a7bc8412de10b02b67bd7bca8e
James Web <149960260+crowereck@users.noreply.github.com>
2023-11-11 06:38:43 +0530

    Update queue.py

--- BgtxD/plugins/tools/queue.py
+++ BgtxD/plugins/tools/queue.py
@@ -1,12 +1,4 @@
-#
-# Copyright (C) 2023-2024 by YukkiOwner@Github, < https://github.com/YukkiOwner >.
-#
-# This file is part of < https://github.com/YukkiOwner/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/YukkiOwner/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-#
+
 
 import asyncio
 import os
@@ -15,7 +7,7 @@ from random import randint
 from pyrogram import filters
 from pyrogram.errors import FloodWait
 from pyrogram.types import CallbackQuery, InputMediaPhoto, Message
-
+from BgtxD import config
 from BgtxD.config import BANNED_USERS
 from BgtxD.strings import get_command
 from BgtxD import app
