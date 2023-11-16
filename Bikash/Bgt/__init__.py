a084136dbe688354efded1a3a924b671a1cf3366
uffyxD <151011010+uffyxD@users.noreply.github.com>
2023-11-17 00:11:52 +0530

    Update __init__.py

--- Bikash/Bgt/__init__.py
+++ Bikash/Bgt/__init__.py
@@ -1,7 +1,4 @@
-
-
 import os
-import sys
 from typing import List
 
 import yaml
@@ -34,7 +31,6 @@ for filename in os.listdir(r"./Bikash/Bgt/langs/"):
         languages["en"] = yaml.safe_load(
             open(r"./Bikash/Bgt/langs/en.yml", encoding="utf8")
         )
-        languages_present["en"] = languages["en"]["name"]
     if filename.endswith(".yml"):
         language_name = filename[:-4]
         if language_name == "en":
@@ -45,12 +41,3 @@ for filename in os.listdir(r"./Bikash/Bgt/langs/"):
         for item in languages["en"]:
             if item not in languages[language_name]:
                 languages[language_name][item] = languages["en"][item]
-    try:
-        languages_present[language_name] = languages[language_name][
-            "name"
-        ]
-    except:
-        print(
-            "There is some issue with the language file inside bot. Please report it to the TeamYukki at @YukkiSupport on Telegram"
-        )
-        sys.exit()
