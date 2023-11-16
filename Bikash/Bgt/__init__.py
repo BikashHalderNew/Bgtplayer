934553e8a244718655e0c8701051f672c5291962
uffyxD <151011010+uffyxD@users.noreply.github.com>
2023-11-16 22:38:17 +0530

    Update __init__.py

--- Bikash/Bgt/__init__.py
+++ Bikash/Bgt/__init__.py
@@ -7,6 +7,9 @@ languages = {}
 languages_present = {}
 
 
+def get_command(value: str) -> List:
+    return commands["command"][value]
+    
 def get_string(lang: str):
     return languages[lang]
 
