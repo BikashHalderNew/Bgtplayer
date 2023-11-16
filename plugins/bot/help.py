1ab59c0a8d1382d53901ae9030c8597c7692597b
uffyxD <151011010+uffyxD@users.noreply.github.com>
2023-11-16 20:38:40 +0530

    Update help.py

--- plugins/bot/help.py
+++ plugins/bot/help.py
@@ -29,7 +29,6 @@ HELP_COMMAND = get_command("HELP_COMMAND")
 @app.on_message(
     filters.command(HELP_COMMAND)
     & filters.private
-    & ~filters.edited
     & ~BANNED_USERS
 )
 @app.on_callback_query(
@@ -74,7 +73,6 @@ async def helper_private(
 @app.on_message(
     filters.command(HELP_COMMAND)
     & filters.group
-    & ~filters.edited
     & ~BANNED_USERS
 )
 @LanguageStart
