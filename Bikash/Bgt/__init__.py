7963619d59a9dfc3e7d82b1bac2d22a29dae54ed
uffyxD <151011010+uffyxD@users.noreply.github.com>
2023-11-16 23:01:53 +0530

    Update __init__.py

--- Bikash/Bgt/__init__.py
+++ Bikash/Bgt/__init__.py
@@ -1,12 +1,4 @@
-#
-# Copyright (C) 2023-2024 by YukkiOwner@Github, < https://github.com/YukkiOwner >.
-#
-# This file is part of < https://github.com/YukkiOwner/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/YukkiOwner/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-#
+
 
 import os
 import sys
@@ -37,7 +29,7 @@ for filename in os.listdir(r"./Bikash/Bgt/langs/en.yml"):
         )
 
 
-for filename in os.listdir(r"./Bikash/Bgt/langs/en.yml:
+for filename in os.listdir(r"./Bikash/Bgt/langs/en.yml"):
     if "en" not in languages:
         languages["en"] = yaml.safe_load(
             open(r"./Bikash/Bgt/langs/en.yml", encoding="utf8")
