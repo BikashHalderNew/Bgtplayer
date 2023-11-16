b81719c123e6c4b35abc1ba7651fa3c8d53200f5
uffyxD <151011010+uffyxD@users.noreply.github.com>
2023-11-16 20:29:16 +0530

    Update mongo.py

--- Bikash/core/mongo.py
+++ Bikash/core/mongo.py
@@ -1,36 +1,14 @@
-# Powered By @BikashHalder @AdityaHalder
+from motor.motor_asyncio import AsyncIOMotorClient
 
-from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
-from pymongo import MongoClient
-from pyrogram import Client
-
-from Bikash import config
+from Bikash.config import MONGO_DB_URI
 
 from ..logging import LOGGER
 
-TEMP_MONGODB = "mongodb+srv://Bikash:Bikashop@bikash.cbkkx4c.mongodb.net/?retryWrites=true&w=majority"
-
-
-if config.MONGO_DB_URI is None:
-    LOGGER(__name__).warning(
-        "No MONGO DB URL found.. Your Bot will work on Bikash's Database"
-    )
-    temp_client = Client(
-        "Bikash",
-        bot_token=config.BOT_TOKEN,
-        api_id=config.API_ID,
-        api_hash=config.API_HASH,
-    )
-    temp_client.start()
-    info = temp_client.get_me()
-    username = info.username
-    temp_client.stop()
-    _mongo_async_ = _mongo_client_(TEMP_MONGODB)
-    _mongo_sync_ = MongoClient(TEMP_MONGODB)
-    mongodb = _mongo_async_[username]
-    pymongodb = _mongo_sync_[username]
-else:
-    _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
-    _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
+LOGGER(__name__).info("Connecting to your Mongo Database...")
+try:
+    _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
     mongodb = _mongo_async_.BgtOwner
-    pymongodb = _mongo_sync_.BgtOwner
+    LOGGER(__name__).info("Connected to your Mongo Database.")
+except:
+    LOGGER(__name__).error("Failed to connect to your Mongo Database.")
+    exit()
