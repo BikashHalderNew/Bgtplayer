cbde699117a41240ce24452bbaa0ca5cee8e4f66
uffyxD <151011010+uffyxD@users.noreply.github.com>
2023-11-17 17:17:10 +0530

    Update stream.py

--- Bikash/utils/stream/stream.py
+++ Bikash/utils/stream/stream.py
@@ -113,9 +113,7 @@ async def stream(
                     original_chat_id,
                     photo=img,
                     caption=_["stream_1"].format(
-                        title[:27],
-                        f"https://t.me/{app.username}?start=info_{vidid}",
-                        duration_min,
+                        
                         user_name,
                     ),
                     reply_markup=InlineKeyboardMarkup(button),
@@ -202,9 +200,7 @@ async def stream(
                     original_chat_id,
                     photo=img,
                     caption=_["stream_1"].format(
-                        title[:27],
-                        f"https://t.me/{app.username}?start=info_{vidid}",
-                        duration_min,
+                        
                         user_name,
                     ),
                     reply_markup=InlineKeyboardMarkup(button),
@@ -376,9 +372,7 @@ async def stream(
                 original_chat_id,
                 photo=img,
                 caption=_["stream_1"].format(
-                    title[:27],
-                    f"https://t.me/{app.username}?start=info_{vidid}",
-                    duration_min,
+                    
                     user_name,
                 ),
                 reply_markup=InlineKeyboardMarkup(button),
