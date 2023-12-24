c7b57f42fa20bb6a4c92ccbc41cb339be740db24
Bikash Halder <115603304+BikashHalderNew@users.noreply.github.com>
2023-12-24 10:31:48 +0530

    Update userinfo.py

--- plugins/extra/userinfo.py
+++ plugins/extra/userinfo.py
@@ -1,17 +1,139 @@
 ## Bikash Halder & Aditya Halder
 
-from Bikash import app
+import os
+from config import BANNED_USERS
 from pyrogram import filters
+from pyrogram.types import Message
+from pyrogram.enums import ParseMode
+from Bikash import app
 
 
-@app.on_message(filters.command("id"))
-def ids(_, message):
-    reply = message.reply_to_message
-    if reply:
-        message.reply_text(
-            f"🥀 𝐃𝐞𝐚𝐫 𝐔𝐬𝐞𝐫 𝐓𝐡𝐢𝐬 𝐈𝐬 𝐔𝐬𝐞𝐫 𝐈𝐧𝐟𝐨 𝐒𝐞𝐞 𝐁𝐞𝐥𝐨𝐰 𝐈𝐧𝐟𝐨 🥀 \n\n🌿 𝐔𝐬𝐞𝐫 𝐈𝐝 🌿: `{reply.from_user.id}`\n\n🥀 𝐅𝐢𝐫𝐬𝐭 𝐍𝐚𝐦𝐞 🍁 : {reply.from_user.first_name} \n\n🥀 𝐔𝐬𝐞𝐫𝐍𝐚𝐦𝐞 🍁 : `@{reply.from_user.username}`\n🥀 𝐂𝐡𝐚𝐭 𝐈𝐝 🥀 : `{message.chat.id}` \n\n𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 [𝐁𝐢𝐤𝐚𝐬𝐡 𝐆𝐚𝐝𝐠𝐞𝐭𝐬 𝐓𝐞𝐜𝐡](https://t.me/BikashGadgetsTech)"
-        )
+n = "\n"
+w = " "
+
+bold = lambda x: f"**{x}:** "
+bold_ul = lambda x: f"**--{x}:**-- "
+mono = lambda x: f"`{x}`{n}"
+
+
+def section(title: str, body: dict, indent: int = 2, underline: bool = False) -> str:
+    text = (bold_ul(title) + n) if underline else bold(title) + n
+    for key, value in body.items():
+        text += (indent * w + bold(key) + ((value[0] + n) if isinstance(value, list) else mono(value)))
+    return text
+
+
+async def get_user_info(user, already=False):
+    if not already:
+        user = await app.get_users(user)
+    if not user.first_name:
+        return ["ᴅᴇʟᴇᴛᴇᴅ ᴀᴄᴄᴏᴜɴᴛ", None]
+    user_id = user.id
+    username = user.username
+    first_name = user.first_name
+    mention = user.mention("ʟɪɴᴋ")
+    mlink = user.mention()
+    uname = ("@" + username) if username else mlink
+    dc_id = user.dc_id
+    photo_id = user.photo.big_file_id if user.photo else None
+    is_scam = user.is_scam
+    is_restricted = user.is_restricted
+    caption = f"""<b><u>Usᴇʀ Fᴜʟʟ Dᴇᴛᴀɪʟs</u> ✨</b>
+
+<b>ɴᴀᴍᴇ:</b> {first_name}
+<b>ᴜsᴇʀɴᴀᴍᴇ:</b> {uname}
+<b>ᴍᴇɴᴛɪᴏɴ:</b> {mention}
+<b>ᴜsᴇʀ ɪᴅ:</b> <code>{user_id}</code>
+<b>ᴜsᴇʀ ᴅᴄ ɪᴅ:</b> <code>{dc_id}</code>
+<b>sᴄᴀᴍ:</b> {is_scam}
+<b>ʀᴇsᴛʀɪᴄᴛᴇᴅ:</b> {is_restricted}"""
+    return [caption, photo_id]
+
+
+async def get_chat_info(chat, already=False):
+    if not already:
+        chat = await app.get_chat(chat)
+    chat_id = chat.id
+    username = chat.username
+    title = chat.title
+    is_scam = chat.is_scam
+    description = chat.description
+    members = chat.members_count
+    is_restricted = chat.is_restricted
+    if username:
+        link = f"@{username}"
     else:
-        message.reply(
-            f"🥀 𝐃𝐞𝐚𝐫 𝐔𝐬𝐞𝐫 𝐓𝐡𝐢𝐬 𝐈𝐬 𝐘𝐨𝐮𝐫 𝐈𝐧𝐟𝐨 𝐒𝐞𝐞 𝐁𝐞𝐥𝐨𝐰 𝐈𝐧𝐟𝐨 🥀 \n\n**🥀 𝐘𝐨𝐮𝐫 𝐈𝐝 🥀**: `{message.from_user.id}`\n**🥀 𝐂𝐡𝐚𝐭 𝐈𝐝 🥀**: `{message.chat.id}`"
-        )
+        try:
+            clink = await app.export_chat_invite_link(chat_id)
+            link = f"<a href={clink}>private</a>"
+        except:
+            link = "ᴘʀɪᴠᴀᴛᴇ"
+            pass
+    photo_id = chat.photo.big_file_id if chat.photo else None
+    caption = f"""<b><u>Cʜᴀᴛ Fᴜʟʟ Dᴇᴛᴀɪʟs</u> ✨</b>
+
+<b>ɴᴀᴍᴇ:</b> {title}
+<b>ᴜsᴇʀɴᴀᴍᴇ:</b> {link}
+<b>ᴄʜᴀᴛ ɪᴅ:</b> <code>{chat_id}
+<b>ᴍᴇᴍʙᴇʀs:</b> <code>{members}</code>
+<b>sᴄᴀᴍ:</b> {is_scam}
+<b>ʀᴇsᴛʀɪᴄᴛᴇᴅ:</b> {is_restricted}
+<b><u>ᴅᴇsᴄʀɪᴘᴛɪᴏɴ</u>:</b>
+<code>{description}</code>"""
+    return [caption, photo_id]
+
+
+@app.on_message(filters.command(["info", "userinfo"]) & ~BANNED_USERS)
+async def info_func(_, message: Message):
+    try:
+        await message.delete()
+    except:
+        pass
+    if message.reply_to_message:
+        user = message.reply_to_message.from_user.id
+    elif not message.reply_to_message and len(message.command) == 1:
+        user = message.from_user.id
+    elif not message.reply_to_message and len(message.command) != 1:
+        user = message.text.split(None, 1)[1]
+
+    m = await message.reply_text("**𝐏ɤ๏ƈɛssɩŋʛ ✨...**")
+    try:
+        info_caption, photo_id = await get_user_info(user)
+    except Exception as e:
+        return await m.edit(str(e))
+
+    if not photo_id:
+        return await m.edit(info_caption, disable_web_page_preview=True)
+    photo = await app.download_media(photo_id)
+
+    await message.reply_photo(photo=photo, caption=info_caption, quote=True, parse_mode=ParseMode.HTML)
+    await m.delete()
+    os.remove(photo)
+
+
+@app.on_message(filters.command("chatinfo") & ~BANNED_USERS)
+async def chat_info_func(_, message: Message):
+    try:
+        await message.delete()
+    except:
+        pass
+    try:
+        if len(message.command) > 2:
+            return await message.reply_text("**ᴜsᴀɢᴇ:** /chat_info [username|id]")
+        if len(message.command) == 1:
+            chat = message.chat.id
+        elif len(message.command) == 2:
+            chat = message.text.split(None, 1)[1]
+            
+        m = await message.reply_text("**𝐏ɤ๏ƈɛssɩŋʛ ✨...**")
+        info_caption, photo_id = await get_chat_info(chat)
+        if not photo_id:
+            return await m.edit(info_caption, disable_web_page_preview=True)
+
+        photo = await app.download_media(photo_id)
+        await message.reply_photo(photo=photo, caption=info_caption, quote=True, parse_mode=ParseMode.HTML)
+        await m.delete()
+        os.remove(photo)
+    except Exception as e:
+        await m.edit(e)
+     
