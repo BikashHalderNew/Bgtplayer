a8045d3b103b81c53f005f2110eda41e4a0282cd
Bikash Halder <115603304+BikashHalderNew@users.noreply.github.com>
2023-12-24 10:07:42 +0530

    Create google.py

---
+++ plugins/extra/google.py
@@ -0,0 +1,206 @@
+from traceback import format_exc
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from search_engine_parser.core.engines.google import Search as GoogleSearch
+from search_engine_parser.core.engines.stackoverflow import \
+    Search as StackSearch
+from search_engine_parser.core.exceptions import NoResultsFound, NoResultsOrTrafficError
+from Bikash import app
+from pyrogram import filters
+
+
+
+
+gsearch = GoogleSearch()
+stsearch = StackSearch()
+
+
+__MODULE__ = "Gᴏᴏɢʟᴇ"
+__HELP__ = """
+⊱ /google : Sᴇᴀʀᴄʜ Aɴʏ Tʜɪɴɢ ( Sᴇᴀʀᴄʜ Bʏ Gᴏᴏɢʟᴇ )
+
+⊱ /gle : Sᴇᴀʀᴄʜ Aɴʏ Tʜɪɴɢ ( Sᴇᴀʀᴄʜ Bʏ Gᴏᴏɢʟᴇ )
+"""
+
+def ikb(rows=None, back=False, todo="start_back"):
+    """
+    rows = pass the rows
+    back - if want to make back button
+    todo - callback data of back button
+    """
+    if rows is None:
+        rows = []
+    lines = []
+    try:
+        for row in rows:
+            line = []
+            for button in row:
+                btn_text = button.split(".")[1].capitalize()
+                button = btn(btn_text, button)  
+                line.append(button)
+            lines.append(line)
+    except AttributeError:
+        for row in rows:
+            line = []
+            for button in row:
+                button = btn(*button)  
+                line.append(button)
+            lines.append(line)
+    except TypeError:
+        # make a code to handel that error
+        line = []
+        for button in rows:
+            button = btn(*button)  # InlineKeyboardButton
+            line.append(button)
+        lines.append(line)
+    if back: 
+        back_btn = [(btn("ʙᴀᴄᴋ", todo))]
+        lines.append(back_btn)
+    return InlineKeyboardMarkup(inline_keyboard=lines)
+
+
+def btn(text, value, type="callback_data"):
+    return InlineKeyboardButton(text, **{type: value})
+
+
+
+
+
+
+@app.on_message(filters.command('google'))
+async def search_(bot: app, message: Message):
+    split = message.text.split(None, 1)
+    if len(split) == 1:
+        return await message.reply_text("**ɢɪᴠᴇ ǫᴜᴇʀʏ ᴛᴏ sᴇᴀʀᴄʜ\n\n Join @BikashGadgetsTech**")
+    to_del = await message.reply_text("**sᴇᴀʀᴄʜɪɴɢ ᴏɴ ɢᴏᴏɢʟᴇ...**")
+    query = split[1]
+    try:
+        result = await gsearch.async_search(query)
+        keyboard = ikb(
+            [
+                [
+                    (
+                        f"{result[0]['titles']}",
+                        f"{result[0]['links']}",
+                        "url",
+                    ),
+                ],
+                [
+                    (
+                        f"{result[1]['titles']}",
+                        f"{result[1]['links']}",
+                        "url",
+                    ),
+                ],
+                [
+                    (
+                        f"{result[2]['titles']}",
+                        f"{result[2]['links']}",
+                        "url",
+                    ),
+                ],
+                [
+                    (
+                        f"{result[3]['titles']}",
+                        f"{result[3]['links']}",
+                        "url",
+                    ),
+                ],
+                [
+                    (
+                        f"{result[4]['titles']}",
+                        f"{result[4]['links']}",
+                        "url",
+                    ),
+                ],
+            ]
+        )
+
+        txt = f"**ʜᴇʀᴇ ᴀʀᴇ ᴛʜᴇ ʀᴇsᴜʟᴛs ᴏғ ʀǫᴜᴇsᴛᴇᴅ : {query.title()} \n\n Join @BikashGadgetsTech**"
+        await to_del.delete()
+        await message.reply_text(txt, reply_markup=keyboard)
+        return
+    except NoResultsFound:
+        await to_del.delete()
+        await message.reply_text("**ɴᴏ ʀᴇsᴜʟᴛ ғᴏᴜɴᴅ ᴄᴏʀʀᴇsᴘᴏɴᴅɪɴɢ ᴛᴏ ʏᴏᴜʀ ǫᴜᴇʀʏ\n\n Join @BikashGadgetsTech**")
+        return
+    except NoResultsOrTrafficError:
+        await to_del.delete()
+        await message.reply_text("****ɴᴏ ʀᴇsᴜʟᴛ ғᴏᴜɴᴅ ᴅᴜᴇ ᴛᴏ ᴛᴏᴏ ᴍᴀɴʏ ᴛʀᴀғғɪᴄ\n\n Join @BikashGadgetsTech**")
+        return
+    except Exception as e:
+        await to_del.delete()
+        await message.reply_text(f"**sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ :\nʀᴇᴘᴏʀᴛ ᴀᴛ ɪᴛ** @Bgt_Chat")
+        print(f"error : {e}")
+        return
+
+
+
+@app.on_message(filters.command('gle'))
+async def stack_search_(bot: app, message: Message):
+    split = message.text.split(None, 1)
+    if len(split) == 1:
+        return await message.reply_text("**ɢɪᴠᴇ ǫᴜᴇʀʏ ᴛᴏ sᴇᴀʀᴄʜ\n\n Join @BikashGadgetsTech**")
+    to_del = await message.reply_text("**sᴇᴀʀᴄʜɪɴɢ ᴏɴ ɢᴏᴏɢʟᴇ...**")
+    query = split[1]
+    try:
+        result = await stsearch.async_search(query)
+        keyboard = ikb(
+            [
+                [
+                    (
+                        f"{result[0]['titles']}",
+                        f"{result[0]['links']}",
+                        "url",
+                    ),
+                ],
+                [
+                    (
+                        f"{result[1]['titles']}",
+                        f"{result[1]['links']}",
+                        "url",
+                    ),
+                ],
+                [
+                    (
+                        f"{result[2]['titles']}",
+                        f"{result[2]['links']}",
+                        "url",
+                    ),
+                ],
+                [
+                    (
+                        f"{result[3]['titles']}",
+                        f"{result[3]['links']}",
+                        "url",
+                    ),
+                ],
+                [
+                    (
+                        f"{result[4]['titles']}",
+                        f"{result[4]['links']}",
+                        "url",
+                    ),
+                ],
+            ]
+        )
+
+        txt = f"**ʜᴇʀᴇ ᴀʀᴇ ᴛʜᴇ ʀᴇsᴜʟᴛs ᴏғ ʀǫᴜᴇsᴛᴇᴅ : {query.title()} \n\n Join @BikashGadgetsTech**"
+        await to_del.delete()
+        await message.reply_text(txt, reply_markup=keyboard)
+        return
+    except NoResultsFound:
+        await to_del.delete()
+        await message.reply_text("**ɴᴏ ʀᴇsᴜʟᴛ ғᴏᴜɴᴅ ᴄᴏʀʀᴇsᴘᴏɴᴅɪɴɢ ᴛᴏ ʏᴏᴜʀ ǫᴜᴇʀʏ\n\n Join @BikashGadgetsTech**")
+        return
+    except NoResultsOrTrafficError:
+        await to_del.delete()
+        await message.reply_text("****ɴᴏ ʀᴇsᴜʟᴛ ғᴏᴜɴᴅ ᴅᴜᴇ ᴛᴏ ᴛᴏᴏ ᴍᴀɴʏ ᴛʀᴀғғɪᴄ\n\n Join @BikashGadgetsTech**")
+        return
+    except Exception as e:
+        await to_del.delete()
+        await msg.reply_text(f"**sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ :\nʀᴇᴘᴏʀᴛ ᴀᴛ ɪᴛ** @Bgt_Chat")
+        print(f"error : {e}")
+        return
+
+
+
