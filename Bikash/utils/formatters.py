b3707c321957bd16754cd1eb4ddc4f5a7c288073
prior-xD <155060158+prior-xD@users.noreply.github.com>
2023-12-31 23:13:32 +0530

    Update formatters.py

--- Bikash/utils/formatters.py
+++ Bikash/utils/formatters.py
@@ -1,19 +1,15 @@
-from typing import Union
-
-from pyrogram.types import Message
+import json
+import subprocess
 
 
 def get_readable_time(seconds: int) -> str:
     count = 0
     ping_time = ""
     time_list = []
-    time_suffix_list = ["s", "m", "h", "days"]
+    time_suffix_list = ["s", "ᴍ", "ʜ", "ᴅᴀʏs"]
     while count < 4:
         count += 1
-        if count < 3:
-            remainder, result = divmod(seconds, 60)
-        else:
-            remainder, result = divmod(seconds, 24)
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
         if seconds == 0 and remainder == 0:
             break
         time_list.append(int(result))
@@ -21,7 +17,7 @@ def get_readable_time(seconds: int) -> str:
     for i in range(len(time_list)):
         time_list[i] = str(time_list[i]) + time_suffix_list[i]
     if len(time_list) == 4:
-        ping_time += time_list.pop() + ", "
+        ping_time += f"{time_list.pop()}, "
     time_list.reverse()
     ping_time += ":".join(time_list)
     return ping_time
@@ -42,11 +38,8 @@ def convert_bytes(size: float) -> str:
 
 async def int_to_alpha(user_id: int) -> str:
     alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
-    text = ""
     user_id = str(user_id)
-    for i in user_id:
-        text += alphabet[int(i)]
-    return text
+    return "".join(alphabet[int(i)] for i in user_id)
 
 
 async def alpha_to_int(user_id_alphabet: str) -> int:
@@ -55,16 +48,12 @@ async def alpha_to_int(user_id_alphabet: str) -> int:
     for i in user_id_alphabet:
         index = alphabet.index(i)
         user_id += str(index)
-    user_id = int(user_id)
-    return user_id
+    return int(user_id)
 
 
 def time_to_seconds(time):
     stringt = str(time)
-    return sum(
-        int(x) * 60**i
-        for i, x in enumerate(reversed(stringt.split(":")))
-    )
+    return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
 
 
 def seconds_to_min(seconds):
@@ -85,6 +74,67 @@ def seconds_to_min(seconds):
         elif s > 0:
             return "00:{:02d}".format(s)
     return "-"
+
+
+def speed_converter(seconds, speed):
+    if str(speed) == "0.5":
+        seconds = seconds * 2
+    if str(speed) == "0.75":
+        seconds = seconds + ((50 * seconds) // 100)
+    if str(speed) == "1.5":
+        seconds = seconds - ((25 * seconds) // 100)
+    if str(speed) == "2.0":
+        seconds = seconds - ((50 * seconds) // 100)
+    collect = seconds
+    if seconds is not None:
+        seconds = int(seconds)
+        d, h, m, s = (
+            seconds // (3600 * 24),
+            seconds // 3600 % 24,
+            seconds % 3600 // 60,
+            seconds % 3600 % 60,
+        )
+        if d > 0:
+            convert = "{:02d}:{:02d}:{:02d}:{:02d}".format(d, h, m, s)
+            return convert, collect
+        elif h > 0:
+            convert = "{:02d}:{:02d}:{:02d}".format(h, m, s)
+            return convert, collect
+        elif m > 0:
+            convert = "{:02d}:{:02d}".format(m, s)
+            return convert, collect
+        elif s > 0:
+            convert = "00:{:02d}".format(s)
+            return convert, collect
+    return "-"
+
+
+def check_duration(file_path):
+    command = [
+        "ffprobe",
+        "-loglevel",
+        "quiet",
+        "-print_format",
+        "json",
+        "-show_format",
+        "-show_streams",
+        file_path,
+    ]
+
+    pipe = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    out, err = pipe.communicate()
+    _json = json.loads(out)
+
+    if "format" in _json:
+        if "duration" in _json["format"]:
+            return float(_json["format"]["duration"])
+
+    if "streams" in _json:
+        for s in _json["streams"]:
+            if "duration" in s:
+                return float(s["duration"])
+
+    return "Unknown"
 
 
 formats = [
