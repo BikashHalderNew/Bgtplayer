5cebba6e7509c02f9d1e3f6490e7ebdac052eb55
ceoxio <161032002+ceoxio@users.noreply.github.com>
2024-02-25 18:37:12 +0530

    Update autoclear.py

--- Bikash/utils/stream/autoclear.py
+++ Bikash/utils/stream/autoclear.py
@@ -2,20 +2,24 @@ import os
 
 from Bikash.config import autoclean
 
-
 async def auto_clean(popped):
     try:
         rem = popped["file"]
-        autoclean.remove(rem)
+        autoclean_copy = autoclean.copy()
+        for item in autoclean_copy:
+            if item == rem:
+                autoclean.remove(item)
+
         count = autoclean.count(rem)
         if count == 0:
             if (
                 "vid_" not in rem
-                or "live_" not in rem
-                or "index_" not in rem
+                and "live_" not in rem
+                and "index_" not in rem
             ):
                 try:
-                    os.remove(rem)
+                    if os.path.exists(rem):
+                        os.remove(rem)
                 except:
                     pass
     except:
