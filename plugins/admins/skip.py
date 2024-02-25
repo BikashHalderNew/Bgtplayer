2670b945637e02245b169949b49480f830bd286f
ceoxio <161032002+ceoxio@users.noreply.github.com>
2024-02-25 18:01:08 +0530

    Update skip.py

--- plugins/admins/skip.py
+++ plugins/admins/skip.py
@@ -1,109 +1,84 @@
-# Powered By @BikashHalder & @AdityaHalder 
-# Join @BikashGadgetsTech For More Updates
-# Join @AdityaCheats For Hacks
-# Join Our Chats @Bgt_Chat & @Adityadiscus 
-
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, Message
-
 from Bikash import config
-from Bikash.config import BANNED_USERS
-from Bikash.Bgt import get_command
 from Bikash import YouTube, app
 from Bikash.core.call import Bikashh
 from Bikash.misc import db
 from Bikash.utils.database import get_loop
 from Bikash.utils.decorators import AdminRightsCheck
-from Bikash.utils.bgtmusic.bk import command
-from Bikash.utils.inline.play import (stream_markup,
-                                          telegram_markup,
-                                          close_keyboard)
+from Bikash.utils.inline import close_markup, stream_markup
 from Bikash.utils.stream.autoclear import auto_clean
 from Bikash.utils.thumbnails import gen_thumb
-
-# Commands
-SKIP_COMMAND = get_command("SKIP_COMMAND")
+from Bikash.config import BANNED_USERS
 
 
 @app.on_message(
-    command(SKIP_COMMAND)
-    & filters.group
-    & ~BANNED_USERS
+    filters.command(["skip", "cskip", "next", "cnext"]) & filters.group & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def skip(cli, message: Message, _, chat_id):
-    if not len(message.command) < 2:
+    if len(message.command) >= 2:
         loop = await get_loop(chat_id)
         if loop != 0:
-            return await message.reply_text(_["admin_12"])
+            return await message.reply_text(_["admin_8"])
         state = message.text.split(None, 1)[1].strip()
-        if state.isnumeric():
-            state = int(state)
-            check = db.get(chat_id)
-            if check:
-                count = len(check)
-                if count > 2:
-                    count = int(count - 1)
-                    if 1 <= state <= count:
-                        for x in range(state):
-                            popped = None
-                            try:
-                                popped = check.pop(0)
-                            except:
-                                return await message.reply_text(
-                                    _["admin_16"]
-                                )
-                            if popped:
-                                if (
-                                    config.AUTO_DOWNLOADS_CLEAR
-                                    == str(True)
-                                ):
-                                    await auto_clean(popped)
-                            if not check:
-                                try:
-                                    await message.reply_text(
-                                        _["admin_10"].format(
-                                            message.from_user.first_name,
-                                            message.chat.title
-                                        ),
-                                        reply_markup=close_keyboard
-                                    )
-                                    await Bikashh.stop_stream(chat_id)
-                                except:
-                                    return
-                                break
-                    else:
-                        return await message.reply_text(
-                            _["admin_15"].format(count)
-                        )
-                else:
-                    return await message.reply_text(_["admin_14"])
-            else:
-                return await message.reply_text(_["queue_2"])
-        else:
-            return await message.reply_text(_["admin_13"])
+        if not state.isnumeric():
+            return await message.reply_text(_["admin_9"])
+        state = int(state)
+        check = db.get(chat_id)
+        if not check:
+            return await message.reply_text(_["queue_2"])
+        count = len(check)
+        if count <= 2:
+            return await message.reply_text(_["admin_10"])
+        count = int(count - 1)
+        if not 1 <= state <= count:
+            return await message.reply_text(_["admin_11"].format(count))
+        for _ in range(state):
+            popped = None
+            try:
+                popped = check.pop(0)
+            except:
+                return await message.reply_text(_["admin_12"])
+            if popped:
+                await auto_clean(popped)
+            if not check:
+                try:
+                    await message.reply_text(
+                        text=_["admin_6"].format(
+                            message.from_user.mention,
+                            message.chat.title,
+                        ),
+                        reply_markup=close_markup(_),
+                    )
+                    await Bikashh.stop_stream(chat_id)
+                except:
+                    return
+                break
     else:
         check = db.get(chat_id)
         popped = None
         try:
-            popped = check.pop(0)
-            if popped:
-                if config.AUTO_DOWNLOADS_CLEAR == str(True):
-                    await auto_clean(popped)
+            if popped := check.pop(0):
+                await auto_clean(popped)
             if not check:
                 await message.reply_text(
-                    _["admin_10"].format(message.from_user.first_name, message.chat.title),
-                    reply_markup=close_keyboard
+                    text=_["admin_6"].format(
+                        message.from_user.mention, message.chat.title
+                    ),
+                    reply_markup=close_markup(_),
                 )
                 try:
-                    return await Bikashh.stop_stream(chat_id)
+                    return await Anony.stop_stream(chat_id)
                 except:
                     return
         except:
             try:
                 await message.reply_text(
-                    _["admin_10"].format(message.from_user.first_name, message.chat.title),
-                    reply_markup=close_keyboard
+                    text=_["admin_6"].format(
+                        message.from_user.mention, message.chat.title
+                    ),
+                    reply_markup=close_markup(_),
                 )
                 return await Bikashh.stop_stream(chat_id)
             except:
@@ -113,26 +88,27 @@ async def skip(cli, message: Message, _, chat_id):
     user = check[0]["by"]
     streamtype = check[0]["streamtype"]
     videoid = check[0]["vidid"]
-    user_id = check[0]["user_id"]
-    duration_min = check[0]["dur"]
     status = True if str(streamtype) == "video" else None
+    db[chat_id][0]["played"] = 0
+    if exis := (check[0]).get("old_dur"):
+        db[chat_id][0]["dur"] = exis
+        db[chat_id][0]["seconds"] = check[0]["old_second"]
+        db[chat_id][0]["speed_path"] = None
+        db[chat_id][0]["speed"] = 1.0
     if "live_" in queued:
         n, link = await YouTube.video(videoid, True)
         if n == 0:
-            return await message.reply_text(
-                _["admin_11"].format(title)
-            )
+            return await message.reply_text(_["admin_7"].format(title))
         try:
             image = await YouTube.thumbnail(videoid, True)
         except:
             image = None
         try:
             await Bikashh.skip_stream(chat_id, link, video=status, image=image)
-        except Exception:
-            return await message.reply_text(_["call_9"])
-
-        button = telegram_markup(_, chat_id)
-        img = await gen_thumb(videoid, user_id)
+        except:
+            return await message.reply_text(_["call_6"])
+        button = stream_markup(_, chat_id)
+        img = await gen_thumb(videoid)
         run = await message.reply_photo(
             photo=img,
             caption=_["stream_1"].format(
@@ -146,9 +122,7 @@ async def skip(cli, message: Message, _, chat_id):
         db[chat_id][0]["mystic"] = run
         db[chat_id][0]["markup"] = "tg"
     elif "vid_" in queued:
-        mystic = await message.reply_text(
-            _["call_10"], disable_web_page_preview=True
-        )
+        mystic = await message.reply_text(_["call_7"], disable_web_page_preview=True)
         try:
             file_path, direct = await YouTube.download(
                 videoid,
@@ -157,25 +131,25 @@ async def skip(cli, message: Message, _, chat_id):
                 video=status,
             )
         except:
-            return await mystic.edit_text(_["call_9"])
+            return await mystic.edit_text(_["call_6"])
         try:
             image = await YouTube.thumbnail(videoid, True)
         except:
             image = None
         try:
             await Bikashh.skip_stream(chat_id, file_path, video=status, image=image)
-        except Exception:
-            return await mystic.edit_text(_["call_9"])
-        button = stream_markup(_, videoid, chat_id)
-        img = await gen_thumb(videoid, user_id)
+        except:
+            return await mystic.edit_text(_["call_6"])
+        button = stream_markup(_, chat_id)
+        img = await gen_thumb(videoid)
         run = await message.reply_photo(
             photo=img,
             caption=_["stream_1"].format(
-                    f"https://t.me/{app.username}?start=info_{videoid}",
-                    title[:23],
-                    check[0]["dur"],
-                    user,
-                ),
+                f"https://t.me/{app.username}?start=info_{videoid}",
+                title[:23],
+                check[0]["dur"],
+                user,
+            ),
             reply_markup=InlineKeyboardMarkup(button),
         )
         db[chat_id][0]["mystic"] = run
@@ -184,9 +158,9 @@ async def skip(cli, message: Message, _, chat_id):
     elif "index_" in queued:
         try:
             await Bikashh.skip_stream(chat_id, videoid, video=status)
-        except Exception:
-            return await message.reply_text(_["call_9"])
-        button = telegram_markup(_, chat_id)
+        except:
+            return await message.reply_text(_["call_6"])
+        button = stream_markup(_, chat_id)
         run = await message.reply_photo(
             photo=config.STREAM_IMG_URL,
             caption=_["stream_2"].format(user),
@@ -195,9 +169,7 @@ async def skip(cli, message: Message, _, chat_id):
         db[chat_id][0]["mystic"] = run
         db[chat_id][0]["markup"] = "tg"
     else:
-        if videoid == "telegram":
-            image = None
-        elif videoid == "soundcloud":
+        if videoid in ["telegram", "soundcloud"]:
             image = None
         else:
             try:
@@ -206,36 +178,36 @@ async def skip(cli, message: Message, _, chat_id):
                 image = None
         try:
             await Bikashh.skip_stream(chat_id, queued, video=status, image=image)
-        except Exception:
-            return await message.reply_text(_["call_9"])
+        except:
+            return await message.reply_text(_["call_6"])
         if videoid == "telegram":
-            button = telegram_markup(_, chat_id)
+            button = stream_markup(_, chat_id)
             run = await message.reply_photo(
                 photo=config.TELEGRAM_AUDIO_URL
                 if str(streamtype) == "audio"
                 else config.TELEGRAM_VIDEO_URL,
-                caption=_["stream_3"].format(
-                    title, check[0]["dur"], user
+                caption=_["stream_1"].format(
+                     title[:23], check[0]["dur"], user
                 ),
                 reply_markup=InlineKeyboardMarkup(button),
             )
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "tg"
         elif videoid == "soundcloud":
-            button = telegram_markup(_, chat_id)
+            button = stream_markup(_, chat_id)
             run = await message.reply_photo(
                 photo=config.SOUNCLOUD_IMG_URL
                 if str(streamtype) == "audio"
                 else config.TELEGRAM_VIDEO_URL,
-                caption=_["stream_3"].format(
-                    title, check[0]["dur"], user
+                caption=_["stream_1"].format(
+                     title[:23], check[0]["dur"], user
                 ),
                 reply_markup=InlineKeyboardMarkup(button),
             )
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "tg"
         else:
-            button = stream_markup(_, videoid, chat_id)
+            button = stream_markup(_, chat_id)
             img = await gen_thumb(videoid)
             run = await message.reply_photo(
                 photo=img,
