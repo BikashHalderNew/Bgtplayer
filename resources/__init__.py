a0da3e06270c2096c7d09dd8583749c49b313925
ceoxio <161032002+ceoxio@users.noreply.github.com>
2024-02-25 13:41:19 +0530

    Update __init__.py

--- resources/__init__.py
+++ resources/__init__.py
@@ -3,6 +3,6 @@ import os
 thumbs = []
 colors = ["white", "black", "red", "orange", "yellow", "green", "cyan", "azure", "blue", "violet", "magenta", "pink"]
 
-for filename in os.listdir("./SankiMusic/resource"):
+for filename in os.listdir("./resources"):
     if filename.endswith("png"):
         thumbs.append(filename[:-4])
