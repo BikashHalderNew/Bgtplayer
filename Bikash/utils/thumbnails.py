cae5919ee6c6c3f1a300766d4275a06550ce6a66
ceoxio <161032002+ceoxio@users.noreply.github.com>
2024-02-26 13:30:31 +0530

    Update thumbnails.py

--- Bikash/utils/thumbnails.py
+++ Bikash/utils/thumbnails.py
@@ -79,7 +79,6 @@ async def gen_thumb(videoid):
     for result in (await results.next())["result"]:
         try:
             title = result["title"]
-            title = re.sub("\W+", " ", title)
             title = title.title()
         except:
             title = "Unsupported Title"
