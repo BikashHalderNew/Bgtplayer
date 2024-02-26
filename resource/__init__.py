cdd77069eb781aaee2cfd54a64053ea425eb19c2
ceoxio <161032002+ceoxio@users.noreply.github.com>
2024-02-26 21:03:12 +0530

    Delete resource directory

--- resource/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-
-thumbs = []
-colors = ["white", "black", "red", "orange", "yellow", "green", "cyan", "azure", "blue", "violet", "magenta", "pink"]
-
-for filename in os.listdir("./resource"):
-    if filename.endswith("png"):
-        thumbs.append(filename[:-4])
