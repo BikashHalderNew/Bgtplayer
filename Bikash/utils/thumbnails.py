c1d6cefd4e2a2987947d9497fa0f215e389f63ed
ceoxio <161032002+ceoxio@users.noreply.github.com>
2024-02-26 18:41:08 +0530

    Update thumbnails.py

--- Bikash/utils/thumbnails.py
+++ Bikash/utils/thumbnails.py
@@ -1,55 +1,14 @@
 import os
 import re
 import textwrap
-import random
 import aiofiles
 import aiohttp
 import numpy as np
 from PIL import Image, ImageChops, ImageDraw, ImageEnhance, ImageFilter, ImageFont
 from youtubesearchpython.__future__ import VideosSearch
 
-from Bikash import config
+from Bikash.config import YOUTUBE_IMG_URL
 from Bikash import app
-YOUTUBE_IMG_URL = [ 
-
-"https://graph.org/file/7ca0ae3fe2e327d3dcc86.jpg",
-"https://graph.org/file/ff0d966aee3123db6ae91.jpg",
-"https://graph.org/file/a39f6f364c34e724ef367.jpg",
-"https://graph.org/file/88fcdd5e044279c0d1747.jpg",
-"https://graph.org/file/fcc2837e0f83cd4d08765.jpg",
-"https://graph.org/file/1fcd0f7d5fdb7e700dca5.jpg",
-"https://graph.org/file/39c27bf76bf8742a148c4.jpg",
-"https://graph.org/file/2c5e6f38ca28687c8c3e8.jpg",
-"https://graph.org/file/71682c8fb277c84031c0f.jpg",
-"https://graph.org/file/c118207cef395ceb196ee.jpg",
-"https://graph.org/file/1b765cd3d9fcc99614a32.jpg",
-"https://graph.org/file/d935b6fbb2e3936b1850a.jpg",
-"https://graph.org/file/e94c4566b9a7cf7d23a4b.jpg",
-"https://graph.org/file/fd2196561ba6e8c4c46a9.jpg",
-"https://graph.org/file/17cbf7cedbc44b85c7bda.jpg",
-"https://graph.org/file/cd72a1c4c3b1d52070a90.jpg",
-"https://graph.org/file/8663d4f19f5d9c30f8ff9.jpg",
-"https://graph.org/file/255e5501d4f8b2e348d87.jpg",
-"https://graph.org/file/cf28d171b08e0590749c7.jpg",
-"https://graph.org/file/a1d7ccd85d58b076f8f88.jpg",
-"https://graph.org/file/aa10da451e1e263105516.jpg",
-"https://graph.org/file/cba9cd9f3fd1bcf841db2.jpg",
-"https://graph.org/file/a06eaee8e9070840947bd.jpg",
-"https://graph.org/file/83f5a1123580bd75f591e.jpg",
-"https://graph.org/file/2f4f60ba8405368505ba2.jpg",
-"https://graph.org/file/18da6db0b032a6c428471.jpg",
-"https://graph.org/file/cd0f7fd0dc68ce8dbe2d4.jpg",
-"https://graph.org/file/eabfa2836ec2e6fb41cd5.jpg",
-"https://graph.org/file/59fb7cd9ea2d1331e75ef.jpg",
-"https://graph.org/file/748e908aaa6cbb6c03402.jpg",
-"https://graph.org/file/a26d09d472ddce5532ca2.jpg",
-"https://graph.org/file/3fb4577387c3934eed027.jpg",
-"https://graph.org/file/e19ab57b45fe4c9765869.jpg",
-"https://graph.org/file/4b9b5d7b6e1a46c55dea1.jpg",
-"https://graph.org/file/d7f9dd74e37f3e4985075.jpg",
-"https://graph.org/file/8fd58648581a8f8d3f19b.jpg",
-    
-    ]
 
 
 def changeImageSize(maxWidth, maxHeight, image):
@@ -71,8 +30,8 @@ def add_corners(im):
 
 
 async def gen_thumb(videoid, user_id):
-    if os.path.isfile(f"cache/{videoid}_{user_id}.png"):
-        return f"cache/{videoid}_{user_id}.png"
+    if os.path.isfile(f"cache/que{videoid}_{user_id}.png"):
+        return f"cache/que{videoid}_{user_id}.png"
     url = f"https://www.youtube.com/watch?v={videoid}"
     try:
         results = VideosSearch(url, limit=1)
@@ -158,15 +117,15 @@ async def gen_thumb(videoid, user_id):
         font = ImageFont.truetype("resource/font2.ttf", 45)
         ImageFont.truetype("resource/font2.ttf", 70)
         arial = ImageFont.truetype("resource/font2.ttf", 30)
-        ImageFont.truetype("resource/font.ttf", 30)
+        ImageFont.truetype("resource/font2.ttf", 30)
         para = textwrap.wrap(title, width=32)
         try:
             draw.text(
-                (450, 25),
-                f"FIND X MUSIC",
+                (455, 25),
+                "ADDED TO QUEUE",
                 fill="white",
-                stroke_width=3,
-                stroke_fill="grey",
+                stroke_width=5,
+                stroke_fill="black",
                 font=font,
             )
             if para[0]:
@@ -198,12 +157,14 @@ async def gen_thumb(videoid, user_id):
             fill="white",
             font=arial,
         )
+
         try:
             os.remove(f"cache/thumb{videoid}.png")
         except:
             pass
-        background.save(f"cache/{videoid}_{user_id}.png")
-        return f"cache/{videoid}_{user_id}.png"
+        file = f"cache/que{videoid}_{user_id}.png"
+        background.save(f"cache/que{videoid}_{user_id}.png")
+        return f"cache/que{videoid}_{user_id}.png"
     except Exception as e:
         print(e)
-        return random.choice(YOUTUBE_IMG_URL)
+        return YOUTUBE_IMG_URL
