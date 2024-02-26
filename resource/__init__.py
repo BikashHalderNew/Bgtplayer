a6fb63fd417991827046903d73593d1c07d90af9
ceoxio <161032002+ceoxio@users.noreply.github.com>
2024-02-26 18:30:51 +0530

    Update __init__.py

--- resource/__init__.py
+++ resource/__init__.py
@@ -3,6 +3,6 @@ import os
 thumbs = []
 colors = ["white", "black", "red", "orange", "yellow", "green", "cyan", "azure", "blue", "violet", "magenta", "pink"]
 
-for filename in os.listdir("./resources"):
+for filename in os.listdir("./resource"):
     if filename.endswith("png"):
         thumbs.append(filename[:-4])
