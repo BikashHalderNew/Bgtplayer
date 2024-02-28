93f23c6ad5dd6b315d4c39a860998b97464c0ed1
Bikash Halder <115603304+BikashHalderNew@users.noreply.github.com>
2024-02-28 08:16:50 +0530

    Update logger.py

--- Bikash/utils/logger.py
+++ Bikash/utils/logger.py
@@ -8,27 +8,27 @@ async def play_logs(message, streamtype):
         if message.chat.username:
             chatusername = f"@{message.chat.username}"
         else:
-            chatusername = "𝐏𝐫𝐢𝐯𝐚𝐭𝐞 𝐂𝐡𝐚𝐭"
+            chatusername = "Private Chat"
         logger_text = f""" ━━━━━━━━━━━━━━━━━━━━━━━     
-**{MUSIC_BOT_NAME} 𝐏𝐥𝐚𝐲 𝐋𝐨𝐠𝐠𝐞𝐫**
+**{MUSIC_BOT_NAME} Play Logger**
 ┏━━━━━━━━━━━━━━━━━┓
-       ༺𝐂𝐡𝐚𝐭 𝐈𝐧𝐟𝐨༻
+       ༺Chat Info༻
 ┗━━━━━━━━━━━━━━━━━┛      
-┣★**𝐂𝐡𝐚𝐭:** {message.chat.title} [`{message.chat.id}`]
-┣★**𝐂𝐡𝐚𝐭 𝐋𝐢𝐧𝐤:** {chatusername}
+┣★**Chat:** {message.chat.title} [`{message.chat.id}`]
+┣★**Chat Link:** {chatusername}
 ┏━━━━━━━━━━━━━━━━━┓
-       ༺𝐔𝐬𝐞𝐫 𝐈𝐧𝐟𝐨༻
+       ༺User Info༻
 ┗━━━━━━━━━━━━━━━━━┛ 
-┣★**𝐔𝐬𝐞𝐫:** {message.from_user.mention}
+┣★**User:** {message.from_user.mention}
 
-┣★**𝐔𝐬𝐞𝐫𝐍𝐚𝐦𝐞:** @{message.from_user.username}
-┣★**𝐈𝐝:** `{message.from_user.id}`
+┣★**UserName:** @{message.from_user.username}
+┣★**Id:** `{message.from_user.id}`
 ┏━━━━━━━━━━━━━━━━━┓
-       ༺𝐏𝐥𝐚𝐲 𝐈𝐧𝐟𝐨༻
+       ༺Play Info༻
 ┗━━━━━━━━━━━━━━━━━┛ 
-┣★**𝐒𝐞𝐚𝐫𝐜𝐡 𝐒𝐨𝐧𝐠:** {message.text}
+┣★**Search Song:** {message.text}
 
-┣★**𝐒𝐫𝐞𝐚𝐦 𝐓𝐲𝐩𝐞:** {streamtype}
+┣★**Stream Type:** {streamtype}
 ━━━━━━━━━━━━━━━━━━━━━━━"""
         if message.chat.id != LOG_GROUP_ID:
             try:
