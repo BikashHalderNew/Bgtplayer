9de63574209ab803854c1573e8327d00bd426c15
zenaku15 <161702193+zenaku15@users.noreply.github.com>
2024-03-03 09:47:56 +0630

    Update git.py

--- Bikash/core/git.py
+++ Bikash/core/git.py
@@ -5,7 +5,7 @@ from typing import Tuple
 from git import Repo
 from git.exc import GitCommandError, InvalidGitRepositoryError
 
-from Bikash import config
+from Zenakusn import config
 
 from ..logging import LOGGER
 
