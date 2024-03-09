7348128222183d03b7152d57df530a99eb263d23
Bikash Halder <115603304+BikashHalderNew@users.noreply.github.com>
2024-03-09 20:50:24 +0530

    Update donate.py

--- plugins/extra/donate.py
+++ plugins/extra/donate.py
@@ -7,7 +7,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 @app.on_message(
     filters.command("donate")
-    & filters.group)
+    )
 async def donate(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://te.legra.ph/file/f73af9a4ffe130a83d8d2.jpg",
