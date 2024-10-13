8b449eeca84bbaac819c49fa81bd1985e305d219
BGT842 <bikashhalder@myyahoo.com>
2024-10-13 21:05:18 +0530

    Update stats.py

--- plugins/tools/stats.py
+++ plugins/tools/stats.py
@@ -43,7 +43,7 @@ async def stats_global(client, message: Message, _):
     upl = stats_buttons(_, True if message.from_user.id in SUDOERS else False)
     await message.reply_photo(
         photo=config.STATS_IMG_URL,
-        caption=_["gstats_11"].format(app.mention),
+        caption=_["gstats_11"],
         reply_markup=upl,
     )
 
